c35c7489465f8563e1c9daeffb134dd90896a034
skull132 <erki.meinberg@gmail.com>
2016-09-18 00:20:04 +0300

    Copyright notice update.

--- lib/ByondPostDLL/ByondPostDLL.cpp
+++ lib/ByondPostDLL/ByondPostDLL.cpp
@@ -1,5 +1,5 @@
 /*
-	Copyright (C) 2016 Oisin Carr
+	Copyright (C) 2016 Oisin Carr & Skull132
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
