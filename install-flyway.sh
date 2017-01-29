0d496ea02ddf166f8a7e7490e29d7e191d40d0fb
skull132 <erki.meinberg@gmail.com>
2017-01-29 19:33:38 +0200

    yaaaaay

--- install-flyway.sh
+++ install-flyway.sh
@@ -9,6 +9,7 @@ else
   curl "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/${FLYWAY_BUILD}/flyway-commandline-${FLYWAY_BUILD}.tar.gz" -o flyway.tar.gz
   tar -xf flyway.tar.gz
   cd "flyway-${FLYWAY_BUILD}"
+  chmod +x flyway
   echo "Flyway installed to $PWD"
 fi
 
