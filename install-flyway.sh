2ff071becbd464f228573cc4cb23d59e4857cfe4
skull132 <erki.meinberg@gmail.com>
2017-01-29 19:47:16 +0200

    More tests.

--- install-flyway.sh
+++ install-flyway.sh
@@ -6,7 +6,8 @@ then
 else
   echo "Setting up Flyway."
   cd "$HOME"
-  curl "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/${FLYWAY_BUILD}/flyway-commandline-${FLYWAY_BUILD}.tar.gz" -o flyway.tar.gz
+  curl "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/4.0.3/flyway-commandline-4.0.3-linux-x64.tar.gz" -o flyway.tar.gz
+#  curl "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/${FLYWAY_BUILD}/flyway-commandline-${FLYWAY_BUILD}.tar.gz" -o flyway.tar.gz
   tar -xf flyway.tar.gz
   cd "flyway-${FLYWAY_BUILD}"
   chmod +x flyway
