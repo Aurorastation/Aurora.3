148b0f85192c8c710a48413b3d65be4a1a08a0b8
skull132 <erki.meinberg@gmail.com>
2017-06-05 22:44:51 +0300

    More shenanigans.

--- install-flyway.sh
+++ install-flyway.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 set -e
-#if [ -f "$HOME/flyway-${FLYWAY_BUILD}/flyway" ];
-#then
-#  echo "Using cached directory."
-#else
-echo "Setting up Flyway."
-cd "$HOME"
-curl "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/4.0.3/flyway-commandline-4.0.3-linux-x64.tar.gz" -o flyway.tar.gz
-#  curl "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/${FLYWAY_BUILD}/flyway-commandline-${FLYWAY_BUILD}.tar.gz" -o flyway.tar.gz
-tar -xf flyway.tar.gz
-cd "flyway-${FLYWAY_BUILD}"
-chmod +x flyway
-echo "Flyway installed to $PWD"
-#fi
+if [ -f "$HOME/flyway-${FLYWAY_BUILD}/flyway" ];
+then
+  echo "Using cached directory."
+else
+  echo "Setting up Flyway."
+  cd "$HOME"
+  curl "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/${FLYWAY_BUILD}/flyway-commandline-${FLYWAY_BUILD}.tar.gz" -o flyway.tar.gz
+  tar -xf flyway.tar.gz
+  cd "flyway-${FLYWAY_BUILD}"
+  chmod +x flyway
+fi
 
