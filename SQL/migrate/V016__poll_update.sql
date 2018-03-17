2910fa949a7ddeb7762a37896879c456ae0fb18f
Werner <1331699+Arrow768@users.noreply.github.com>
2018-03-17 03:53:07 +0100

    Adds a gui to create polls
    Completely stolen from vg

---
+++ SQL/migrate/V016__poll_update.sql
@@ -0,0 +1,7 @@
+--
+-- Implemented in PR FILLMEOUT
+--
+
+ALTER TABLE `ss13_poll_question`
+	ADD COLUMN `createdby_ckey` VARCHAR(50) NULL DEFAULT NULL AFTER `adminonly`,
+	ADD COLUMN `createdby_ip` VARCHAR(50) NULL DEFAULT NULL AFTER `createdby_ckey`;
