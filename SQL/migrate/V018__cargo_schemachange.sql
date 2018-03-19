6dbc35466d7efb2cd8ef651132a88412e72036b5
Werner <Arrow768@users.noreply.github.com>
2018-03-19 15:25:46 +0100

    Fix the ;

--- SQL/migrate/V018__cargo_schemachange.sql
+++ SQL/migrate/V018__cargo_schemachange.sql
@@ -11,6 +11,6 @@ ALTER TABLE `ss13_cargo_items`
 	ADD COLUMN `price` INT(11) NOT NULL AFTER `categories`,
 	ADD COLUMN `items` TEXT NULL COMMENT 'JSON list of all the items and their attributes' AFTER `price`,
 	CHANGE COLUMN `amount` `item_mul` INT(10) UNSIGNED NOT NULL DEFAULT '1' AFTER `items`,
-	CHANGE COLUMN `access` `access` INT(10) UNSIGNED NOT NULL DEFAULT '0' AFTER `item_mul`;
+	CHANGE COLUMN `access` `access` INT(10) UNSIGNED NOT NULL DEFAULT '0' AFTER `item_mul`,
 	CHANGE COLUMN `suppliers` `suppliers_old` TEXT NULL COMMENT 'JSON list of suppliers' COLLATE 'utf8_bin' AFTER `deleted_at`,
 	CHANGE COLUMN `path` `path_old` VARCHAR(150) NOT NULL COLLATE 'utf8_bin' AFTER `suppliers_old`;
