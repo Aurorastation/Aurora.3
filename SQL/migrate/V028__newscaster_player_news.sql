67499f9d2217ac16eab3ea77a7386744258bc8b5
Werner <Arrow768@users.noreply.github.com>
2018-05-22 21:04:02 +0200

    the ic timestamp can be null aswell

--- SQL/migrate/V028__newscaster_player_news.sql
+++ SQL/migrate/V028__newscaster_player_news.sql
@@ -4,7 +4,7 @@
 ALTER TABLE `ss13_news_stories`
 	CHANGE COLUMN `time_stamp` `publish_at` DATETIME NULL DEFAULT NULL AFTER `is_admin_message`,
 	ADD COLUMN `publish_until` DATETIME NULL DEFAULT NULL AFTER `publish_at`,
-	ADD COLUMN `ic_timestamp` DATETIME NOT NULL AFTER `publish_until`,
+	ADD COLUMN `ic_timestamp` DATETIME NULL DEFAULT NULL AFTER `publish_until`,
 	ADD COLUMN `approved_by` VARCHAR(50) NULL DEFAULT NULL AFTER `created_at`,
 	ADD COLUMN `approved_at` DATETIME NULL DEFAULT NULL AFTER `approved_by`,
 	ADD COLUMN `url` VARCHAR(250) NULL DEFAULT NULL AFTER `publish_until`;
