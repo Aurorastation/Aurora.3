Aurorastation/Aurora.3

commit 5d983a1b59b19deb1e2eeca5af48ade379f9904a

it was a mistake (#4913)

code/game/turfs/simulated.dm
@@ -89,27 +89,22 @@
 			var/mob/living/carbon/human/H = M
 			// Tracking blood
 			var/list/bloodDNA = list()
-			var/will_track = 0
 			var/bloodcolor=""
 			if(H.shoes)
 				var/obj/item/clothing/shoes/S = H.shoes
 				if(istype(S))
 					S.handle_movement(src,(H.m_intent == "run" ? 1 : 0))
 					if(S.track_blood)
-						if(S.blood_DNA)
-							bloodDNA = S.blood_DNA
+						bloodDNA = S.blood_DNA
 						bloodcolor=S.blood_color
 						S.track_blood--
-						will_track = 1
 			else
-				if(H.track_blood)
-					if(H.feet_blood_DNA)
-						bloodDNA = H.feet_blood_DNA
+				if(H.track_blood && H.feet_blood_DNA)
+					bloodDNA = H.feet_blood_DNA
 					bloodcolor = H.feet_blood_color
 					H.track_blood--
-					will_track = 1
 
-			if(will_track)
+			if(bloodDNA)
 				src.AddTracks(/obj/effect/decal/cleanable/blood/tracks/footprints,bloodDNA,H.dir,0,bloodcolor) // Coming
 				var/turf/simulated/from = get_step(H,reverse_direction(H.dir))
 				if(istype(from) && from)

code/modules/mining/mine_turf_types.dm
@@ -100,38 +100,6 @@
 	if (prob(20))
 		add_overlay("asteroid[rand(0, 9)]", TRUE)
 
-
-
-/turf/simulated/floor/asteroid/ash/Entered(atom/A, atom/OL)
-	..()
-
-	if(ishuman(A))
-		var/mob/living/carbon/human/H = A
-		var/obj/item/organ/external/l_foot = H.get_organ("l_foot")
-		var/obj/item/organ/external/r_foot = H.get_organ("r_foot")
-		var/hasfeet = 1
-		if((!l_foot || l_foot.is_stump()) && (!r_foot || r_foot.is_stump()))
-			hasfeet = 0
-		if(H.shoes && !H.buckled)//Adding ash to shoes
-			var/obj/item/clothing/shoes/S = H.shoes
-			if(istype(S))
-				S.blood_color = "#6C6564"
-				S.track_blood = max(12,S.track_blood)
-
-				if(!S.blood_overlay)
-					S.generate_blood_overlay()
-				if(S.blood_overlay && S.blood_overlay.color != "#6C6564")
-					S.cut_overlay(S.blood_overlay, TRUE)
-
-				S.blood_overlay.color = "#6C6564"
-				S.add_overlay(S.blood_overlay, TRUE)
-
-		else if (hasfeet)//Or feet
-			H.feet_blood_color = "#6C6564"
-			H.track_blood = max(12,H.track_blood)
-
-		H.update_inv_shoes(1)
-
 /turf/simulated/floor/asteroid/ash/rocky
 	name = "rocky ash"
 	icon_state = "rockyash"

commit d6f4754e118be0f763ea0eae9d21d51b00af2c22

Rectifies semen feet (#4952)
This fixes a bug introduced by #4913

code/game/turfs/simulated.dm
@@ -88,13 +88,13 @@
 		if(istype(M, /mob/living/carbon/human))
 			var/mob/living/carbon/human/H = M
 			// Tracking blood
-			var/list/bloodDNA = list()
+			var/list/bloodDNA = null
 			var/bloodcolor=""
 			if(H.shoes)
 				var/obj/item/clothing/shoes/S = H.shoes
 				if(istype(S))
 					S.handle_movement(src,(H.m_intent == "run" ? 1 : 0))
-					if(S.track_blood)
+					if(S.track_blood && S.blood_DNA)
 						bloodDNA = S.blood_DNA
 						bloodcolor=S.blood_color
 						S.track_blood--