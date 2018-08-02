8381602e75c430bab45acd70c54a17df1804a760
Karolis2011 <kundrotaskarolis3@gmail.com>
2018-08-02 17:28:39 +0300

    I forgot to handle this

--- vueui/src/components/header/default.vue
+++ vueui/src/components/header/default.vue
@@ -9,53 +9,52 @@
 
 <script>
 export default {
-    name: "header-default",
-    data () {
-        return this.$root.$data
-    },
-    computed: {
-        statusClass() {
-            if (this.status == 2) return 'uiStatusGood'
-            if (this.status == 1) return 'uiStatusAverage'
-            return 'uiStatusBad'
-        }
+  data () {
+    return this.$root.$data
+  },
+  computed: {
+    statusClass() {
+      if (this.status == 2) return 'uiStatusGood'
+      if (this.status == 1) return 'uiStatusAverage'
+      return 'uiStatusBad'
     }
+  }
 }
 </script>
 
 <style lang="scss">
 .uiTitleWrapper {
-    margin: -8px -8px 10px;
-    position: relative;
-    padding-bottom: 8px;
-    float: none;
-    background-color: #b9b9b9;
+  margin: -8px -8px 10px;
+  position: relative;
+  padding-bottom: 8px;
+  float: none;
+  background-color: #b9b9b9;
 }
 
 .uiTitleText {
-    position: relative;
-    display: inline-block;
-    top: 4px;
-    left: 18px;
-    width: 66%;
-    width: calc(100% - 36px - 42px - 18px);
-    font-size: 16px;
-    line-height: 20px;
-    vertical-align: middle;
+  position: relative;
+  display: inline-block;
+  top: 4px;
+  left: 18px;
+  width: 66%;
+  width: calc(100% - 36px - 42px - 18px);
+  font-size: 16px;
+  line-height: 20px;
+  vertical-align: middle;
 }
 
 .uiTitle.icon {
-    padding: 6px 8px 6px 42px;
-    background-position: 2px 50%;
-    background-repeat: no-repeat;
+  padding: 6px 8px 6px 42px;
+  background-position: 2px 50%;
+  background-repeat: no-repeat;
 }
 
 .uiStatusIcon {
-    position: relative;
-    display: inline-block;
-    top: 4px;
-    left: 12px;
-    vertical-align: middle;
+  position: relative;
+  display: inline-block;
+  top: 4px;
+  left: 12px;
+  vertical-align: middle;
 }
 </style>
 
