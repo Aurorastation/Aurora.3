2fd5d7a81a9622fc810465236633b5eba91abd1c
Sindorman <mikebykhovtsev@gmail.com>
2018-11-15 11:50:32 -0800

    Wrong path

--- tools/mapmerge2/travis_mapcheck.py
+++ tools/mapmerge2/travis_mapcheck.py
@@ -106,7 +106,7 @@ def merge_map(new_map, old_map, name):
 if __name__ == '__main__':
 
     list_of_files = list()
-    for root, directories, filenames in os.walk("/maps/"):
+    for root, directories, filenames in os.walk("../../maps/"):
         for filename in [f for f in filenames if f.endswith(".dmm")]:
             list_of_files.append(str(pathlib.Path(root, filename)))
             print(str(pathlib.Path(root, filename)))
