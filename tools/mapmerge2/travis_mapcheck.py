ccf3ffc66471177d53aaed4a993263dea10131fb
Sindorman <mikebykhovtsev@gmail.com>
2018-11-15 12:27:55 -0800

    Let's try 3.4 again

--- tools/mapmerge2/travis_mapcheck.py
+++ tools/mapmerge2/travis_mapcheck.py
@@ -110,7 +110,6 @@ if __name__ == '__main__':
     for root, directories, filenames in os.walk("/home/travis/build/Aurorastation/Aurora.3/maps/"):
         for filename in [f for f in filenames if f.endswith(".dmm")]:
             list_of_files.append(str(pathlib.Path(root, filename)))
-            print(str(pathlib.Path(root, filename)))
 
     for fname in list_of_files:
         shutil.copyfile(fname, fname + ".backup")
