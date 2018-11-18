e62f38b266492c269fb8053854deffbc70b68cc6
Sindorman <mikebykhovtsev@gmail.com>
2018-11-17 20:45:05 -0800

    Lets test this out

--- tools/mapmerge2/travis_mapcheck.py
+++ tools/mapmerge2/travis_mapcheck.py
@@ -7,49 +7,31 @@ from dmm import *
 
 class DMM_TRAVIS(DMM):
 
-    def to_file(self, fname, tgm = True):
+    def _presave_checks(self, fname):
+        # last-second handling of bogus keys to help prevent and fix broken maps
         try:
-            self._presave_checks(fname)
-            with open(fname, 'w', newline='\n', encoding=ENCODING) as f:
-                (save_tgm if tgm else save_dmm)(self, f)
+            self._ensure_free_keys(0)
+            max_key = max_key_for(self.key_length)
         except KeyTooLarge:
             print("\nKey is too large, run mapmerge2 on {} locally and fix errors.".format(fname))
             exit(1)
-
-    def _presave_checks(self, fname):
-        # last-second handling of bogus keys to help prevent and fix broken maps
-        self._ensure_free_keys(0)
-        max_key = max_key_for(self.key_length)
         bad_keys = {key: 0 for key in self.dictionary.keys() if key > max_key}
         if bad_keys:
-            print("Bad keys detected, please run mapmerge2 on {} locally and fix errors.".format(fname))
+            print("\nBad keys detected, please run mapmerge2 on {} locally and fix errors.".format(fname))
             exit(1)
 
-def merge_map(new_map, old_map, name):
-    if new_map.key_length != old_map.key_length:
-        print("Warning: Key lengths differ, taking new map")
-        print("  Old: {}".format(old_map.key_length))
-        print("  New: {}".format(new_map.key_length))
-        print("\nIn file {}".format(name))
-        exit(1)
+def map_check(map, name):
 
-    if new_map.size != old_map.size:
-        print("Warning: Map dimensions differ, taking new map")
-        print("  Old: {}".format(old_map.size))
-        print("  New: {}".format(new_map.size))
-        print("\nIn file {}".format(name))
-        exit(1)
-
-    key_length, size = old_map.key_length, old_map.size
+    key_length, size = map.key_length, map.size
     merged = DMM_TRAVIS(key_length, size)
-    merged.dictionary = old_map.dictionary.copy()
+    merged.dictionary = map.dictionary.copy()
 
     known_keys = dict()  # mapping fron 'new' key to 'merged' key
-    unused_keys = set(old_map.dictionary.keys())  # keys going unused
+    unused_keys = set(map.dictionary.keys())  # keys going unused
 
     # step one: parse the new version, compare it to the old version, merge both
-    for z, y, x in new_map.coords_zyx:
-        new_key = new_map.grid[x, y, z]
+    for z, y, x in map.coords_zyx:
+        new_key = map.grid[x, y, z]
         # if this key has been processed before, it can immediately be merged
         try:
             merged.grid[x, y, z] = known_keys[new_key]
@@ -60,9 +42,9 @@ def merge_map(new_map, old_map, name):
         def select_key(assigned):
             merged.grid[x, y, z] = known_keys[new_key] = assigned
 
-        old_key = old_map.grid[x, y, z]
-        old_tile = old_map.dictionary[old_key]
-        new_tile = new_map.dictionary[new_key]
+        old_key = map.grid[x, y, z]
+        old_tile = map.dictionary[old_key]
+        new_tile = map.dictionary[new_key]
 
         # this tile is the exact same as before, so the old key is used
         if new_tile == old_tile:
@@ -76,7 +58,7 @@ def merge_map(new_map, old_map, name):
             unused_keys.remove(newold_key)
 
         # the tile is brand new and it needs a new key, but if the old key isn't being used any longer it can be used instead
-        elif old_tile not in new_map.dictionary.inv and old_key in unused_keys:
+        elif old_tile not in map.dictionary.inv and old_key in unused_keys:
             merged.dictionary[old_key] = new_tile
             select_key(old_key)
             unused_keys.remove(old_key)
@@ -89,24 +71,8 @@ def merge_map(new_map, old_map, name):
 
     # step two: delete unused keys
     if unused_keys:
-        for key in unused_keys:
-            del merged.dictionary[key]
         print("Error: {} unused dictionary keys. Please run mapmerge2 on {} locally to trim them.".format(len(unused_keys), name))
         exit(1)
-
-    # sanity check: that the merged map equals the new map
-    for z, y, x in new_map.coords_zyx:
-        new_tile = new_map.dictionary[new_map.grid[x, y, z]]
-        merged_tile = merged.dictionary[merged.grid[x, y, z]]
-        if new_tile != merged_tile:
-            print("Error: the map has been mangled! This is a mapmerge bug!")
-            print("At {},{},{}.".format(x, y, z))
-            print("Should be {}".format(new_tile))
-            print("Instead is {}".format(merged_tile))
-            print("map name: {}".format(name))
-            exit(1)
-
-    return merged
 
 if __name__ == '__main__':
 
@@ -116,9 +82,6 @@ if __name__ == '__main__':
             list_of_files.append(str(pathlib.Path(root, filename)))
 
     for fname in list_of_files:
-        shutil.copyfile(fname, fname + ".backup")
-        old_map = DMM_TRAVIS.from_file(fname + ".backup")
-        new_map = DMM_TRAVIS.from_file(fname)
-        merge_map(new_map, old_map, fname).to_file(fname, 1)
-        os.remove(fname + ".backup")
+        map = DMM_TRAVIS.from_file(fname)
+        map_check(map, fname)._presave_checks(fname)
     print("Maps scanning complete, no issues were found.")
