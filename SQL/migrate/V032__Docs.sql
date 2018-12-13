3c7e2fb5d10d61322b6e33437ead9e41416923aa
MarinaGryphon <ryfoxx93@gmail.com>
2018-12-13 15:02:36 -0600

    First attempt at an SQL migration.
    Oh God what am I doing.
    Needs to process the `tags` entry into a list clientside, and the same for storing. This means more work on the inevitable WI side.

---
+++ SQL/migrate/V032__Docs.sql
@@ -0,0 +1,15 @@
+--
+-- Create the Documents database.
+--
+
+CREATE TABLE `ss13_documents` (
+	`name` VARCHAR(100) NOT NULL COLLATE 'utf8_bin',
+	`title` VARCHAR(26) NOT NULL COLLATE 'utf8_bin',
+	`chance` FLOAT UNSIGNED NOT NULL DEFAULT '1',
+	`content` VARCHAR(3072) NOT NULL COLLATE 'utf8_bin',
+	`tags` VARCHAR(1024) NOT NULL COLLATE 'utf8_bin'
+	PRIMARY KEY (`name`)
+)
+COLLATE='utf8_bin'
+ENGINE=InnoDB
+;
