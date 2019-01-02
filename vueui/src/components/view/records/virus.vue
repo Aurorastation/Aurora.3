538dadbf962a43a3f5b41351fac02b704d5c2a8a
Karolis2011 <kundrotaskarolis3@gmail.com>
2019-01-02 19:49:42 +0200

    Added virus note editing

--- vueui/src/components/view/records/virus.vue
+++ vueui/src/components/view/records/virus.vue
@@ -2,7 +2,7 @@
   <div>
     <vui-item label="ID:">{{ active_virus.id }}</vui-item>
     <vui-item label="Description:" v-html="active_virus.description"/>
-    <vui-item label="Notes:">{{ active_virus.notes }}</vui-item>
+    <vui-item label="Notes:"><view-records-field :editable="(editable & 32) > 0" path="active_virus.notes"><textarea v-model="$root.$data.state.editingvalue"/></view-records-field></vui-item>
   </div>
 </template>
 
