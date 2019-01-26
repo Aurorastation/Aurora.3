42016694046c4d7853e37adfb7a811dd96586eb2
dragoslava <dragoslava@users.noreply.github.com>
2019-01-25 21:47:38 -0800

    aaaaa

--- vueui/src/components/view/quikpay/main.vue
+++ vueui/src/components/view/quikpay/main.vue
@@ -3,17 +3,17 @@
     <div v-for="(price, name) in items" :key="name">
       {{ name }}: {{ price }}
       <vui-input-numeric v-model="selection[name]" width="2em"/>
-    <div v-if="editmode == 1">
-    <vui-button :params="{ remove: name }" width="3em">Delete</vui-button>
+      <div v-if="editmode == 1">
+        <vui-button :params="{ remove: name }" width="3em">Delete</vui-button>
+      </div>
     </div>
-  </div>
     <div>
-    <div v-if="editmode == 1">
-      <input v-model="tmp_name">
-      <vui-input-numeric v-model="tmp_price" width="3em" :button-count="2"/>
-      <vui-button :params="{ add: {name: tmp_name, price: tmp_price }}">Add</vui-button>
+      <div v-if="editmode == 1">
+        <input v-model="tmp_name">
+        <vui-input-numeric v-model="tmp_price" width="3em" :button-count="2"/>
+        <vui-button :params="{ add: {name: tmp_name, price: tmp_price }}">Add</vui-button>
+      </div>
     </div>
-  </div>
     <vui-button :params="{ confirm: selection }">Confirm payment</vui-button>
     <vui-button :params="{ unlock: 1 }" width="3em">Toggle Menu Lock</vui-button>
   </div>
