0ccbcb3c6fb4ebc55a8db05ea7807c30ab60f1a2
Karolis2011 <kundrotaskarolis3@gmail.com>
2019-01-27 12:35:59 +0200

    Fixes and updates

--- vueui/src/components/view/records/list.vue
+++ vueui/src/components/view/records/list.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-for="record in allrecords" :key="record.id" v-if="record">
+    <div v-for="record in allrecords" :key="record.id">
       <vui-button :params="{ setactive: record.id}" @click="activeview = defaultview" push-state>{{ record.id }}: {{ record.name }}</vui-button>
     </div>
     <vui-button :params="{ newrecord: 1 }" @click="activeview = defaultview" push-state>New record</vui-button>
