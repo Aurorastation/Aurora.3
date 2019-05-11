15ad0755c7a903b7709fe8cada9ed6e1a43ea815
Karolis <Karolis2011@users.noreply.github.com>
2019-05-11 22:33:11 +0300

    VueUi based voting (#6405)

---
+++ vueui/src/components/header/minimal.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="uiTitleWrapper" unselectable="on">
+    <slot/>
+    <div class="uiTitleText" unselectable="on">{{ title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return this.$root.$data
+  }
+}
+</script>
