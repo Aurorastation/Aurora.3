fa986d7f247d6261f28622d0c591f966fe11dbbd
Karolis2011 <Karolis2011@users.noreply.github.com>
2019-05-12 04:12:27 +0300

    Fixes wording and brings back secret.

--- vueui/src/components/view/misc/voting.vue
+++ vueui/src/components/view/misc/voting.vue
@@ -26,11 +26,11 @@
         <li>
           <vui-button :disabled="!isstaff && !allow_vote_restart" :params="{action: 'crew_transfer'}">Crew Transfer</vui-button>
           <span v-if="(isstaff || allow_vote_restart) && is_code_red">(Disallowed, Code Red or above)</span>
-          <vui-button :class="{on: allow_vote_restart}" v-if="isstaff" :params="{action: 'toggle_restart'}">Toggle Restart</vui-button>
+          <vui-button :class="{on: allow_vote_restart}" v-if="isstaff" :params="{action: 'toggle_restart'}">Toggle Restart / Crew Transfer voting</vui-button>
         </li>
         <li>
           <vui-button :disabled="!isstaff && !allow_vote_mode" :params="{action: 'gamemode'}">GameMode</vui-button>
-          <vui-button :class="{on: allow_vote_mode}" v-if="isstaff" :params="{action: 'toggle_gamemode'}">Toggle GameMode</vui-button>
+          <vui-button :class="{on: allow_vote_mode}" v-if="isstaff" :params="{action: 'toggle_gamemode'}">Toggle GameMode voting</vui-button>
         </li>
         <li>
           <vui-button :disabled="!allow_extra_antags" :params="{action: 'add_antagonist'}">Add Antagonist Type</vui-button>
