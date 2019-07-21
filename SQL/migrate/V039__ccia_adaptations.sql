6e8415f6eb0b719d3739b52c07bb53293d38da63
Werner <Arrow768@users.noreply.github.com>
2019-07-22 01:52:38 +0200

    Fixes SQL Migration Error

--- SQL/migrate/V039__ccia_adaptations.sql
+++ SQL/migrate/V039__ccia_adaptations.sql
@@ -7,7 +7,7 @@ ALTER TABLE `ss13_ccia_reports`
 ALTER TABLE `ss13_ccia_reports`
 	ADD COLUMN `public_topic` VARCHAR(200) NULL DEFAULT NULL AFTER `title`,
 	ADD COLUMN `internal_topic` VARCHAR(200) NULL DEFAULT NULL AFTER `public_topic`,
-	ADD COLUMN `game_id` VARCHAR(20) NOT NULL AFTER `internal_topic`,;
+	ADD COLUMN `game_id` VARCHAR(20) NOT NULL AFTER `internal_topic`;
 ALTER TABLE `ss13_ccia_reports`
 	CHANGE COLUMN `status` `status` ENUM('new','in progress','review required','approved','rejected','completed') NOT NULL COLLATE 'utf8mb4_unicode_ci' AFTER `game_id`;
 
