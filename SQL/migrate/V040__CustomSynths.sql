8f1683979741373e109351333834b3f9f7f042d6
LadyFowls <dragoslava@users.noreply.github.com>
2019-08-03 14:04:13 -0700

    eeeeeee

--- SQL/migrate/V040__CustomSynths.sql
+++ SQL/migrate/V040__CustomSynths.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE `ss13_customsynths` (
 	`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`synthckey` VARCHAR(100) NOT NULL,
+	`synthckey` varchar(32) NOT NULL,
 	`synthicon` VARCHAR(26) NOT NULL,
 	`ainame` VARCHAR(100) NOT NULL,
 	`aichassisicon` VARCHAR(100) NOT NULL,
