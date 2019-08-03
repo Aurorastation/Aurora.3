18cfc44d1803bcc3ca9a20ebef63cfbf451a25fc
LadyFowls <dragoslava@users.noreply.github.com>
2019-08-03 14:28:08 -0700

    skull test

--- SQL/migrate/V040__CustomSynths.sql
+++ SQL/migrate/V040__CustomSynths.sql
@@ -3,16 +3,15 @@
 --
 
 CREATE TABLE `ss13_customsynths` (
-	`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`synthckey` varchar(32) CHARACTER SET latin1 NOT NULL,
-	`synthicon` VARCHAR(26) NOT NULL,
-	`ainame` VARCHAR(100) NOT NULL,
-	`aichassisicon` VARCHAR(100) NOT NULL,
-	`aiholoicon` VARCHAR(100) NOT NULL,
-	`paiicon` VARCHAR(100) NOT NULL,
-	PRIMARY KEY (`id`),
+    `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `synthckey` varchar(32) NOT NULL,
+    `synthicon` VARCHAR(26) NOT NULL,
+    `ainame` VARCHAR(100) NOT NULL,
+    `aichassisicon` VARCHAR(100) NOT NULL,
+    `aiholoicon` VARCHAR(100) NOT NULL,
+    `paiicon` VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`id`),
     CONSTRAINT `fk_ss13_custom_synths_ss13_players` FOREIGN KEY (`synthckey`) REFERENCES `ss13_player` (`ckey`) ON DELETE CASCADE ON UPDATE CASCADE
 )
-COLLATE='utf8_bin'
 ENGINE=InnoDB
 ;
