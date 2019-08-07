190a34757d8ea9a3dd43422b601586ffd767ac74
skull132 <erki.meinberg@gmail.com>
2019-08-07 22:43:49 +0300

    Fix migration

--- SQL/migrate/V040__CustomSynths.sql
+++ SQL/migrate/V040__CustomSynths.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE `ss13_customsynths` (
     `synthname` VARCHAR(128) NOT NULL,
-    `synthckey` varchar(32) NOT NULL,
+    `synthckey` varchar(32) CHARACTER SET latin1 NOT NULL,
     `synthicon` VARCHAR(26) NOT NULL,
     `aichassisicon` VARCHAR(100) NOT NULL,
     `aiholoicon` VARCHAR(100) NOT NULL,
