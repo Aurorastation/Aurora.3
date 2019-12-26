452cfe386bb138c993183dffce697389c6301cf5
Karolis2011 <Karolis2011@users.noreply.github.com>
2019-12-26 19:09:40 +0200

    aaaa

--- vueui/src/components/view/mcomputer/medical/sensors.vue
+++ vueui/src/components/view/mcomputer/medical/sensors.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
-    
+    <div class="table">
+      <div class="header">
+        <div class="header-item">Name</div>
+        <div class="header-item">Pulse</div>
+        <div class="header-item">Pressure</div>
+        <div class="header-item">Oxy</div>
+        <div class="header-item">Temp</div>
+        <div class="header-item">Location</div>
+        <div class="header-item"></div>
+      </div>
+      <div class="sensor" v-for="sensor in crewmembers" :key="sensor.ref">
+        <div class="item">{{ sensor.name }} ({{ sensor.ass }})</div>
+        <div class="item center" :class="getPulseClass(sensor.tpulse)">{{ sensor.pulse }}</div>
+        <div class="item center">{{ sensor.pressure }}</div>
+        <div class="item center" :class="getOxyClass(sensor.oxyg)">{{ toOxiLabel(sensor.oxyg) }}</div>
+        <div class="item center">{{ roundTemp(sensor.bodytemp) }}</div>
+        <div class="item rigth" v-if="sensor.stype > 2">{{sensor.area}} ({{sensor.x}}, {{sensor.y}}, {{sensor.z}})</div>
+        <div class="item rigth" v-else>Not Available</div>
+        <div class="item rigth" v-if="isAI"><vui-button :params="{'Track': sensor.ref}">Track</vui-button></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,6 +28,89 @@
 export default {
   data() {
     return this.$root.$data.state
+  },
+  methods: {
+    roundTemp(t) {
+      return Math.round(t * 10) / 10
+    },
+    toOxiLabel(value) {
+      switch (value) {
+        case 5:
+          return "increased"
+        case 4:
+          return "normal"
+        case 3:
+          return "low"
+        case 2:
+          return "very low"
+        case 1:
+          return "extremely low"
+        default:
+          return "N/A";
+      }
+    },
+    getOxyClass(value) {
+      switch (value) {
+        case 5:
+          return "highlight"
+        case 4:
+          return "good"
+        case 3:
+          return "average"
+        case 2:
+          return "bad"
+        case 1:
+          return "bad"
+        default:
+          return "neutral";
+      }
+    },
+    getPulseClass(tpulse) {
+      switch (tpulse) {
+        case 0:
+          return "bad"
+        case 1:
+          return "average"
+        case 2:
+          return "good"
+        case 3:
+          return "highlight"
+        case 4:
+          return "average"
+        case 5:
+          return "bad"
+        default:
+          return "neutral";
+      }
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.table {
+  display: table;
+  width: 100%;
+
+  .header {
+    text-align: center;
+    font-weight: bold;
+  }
+  .header, .sensor {
+    display: table-row;
+    .header-item, .item {
+      display: table-cell;
+      padding: 1px;
+      vertical-align: middle;
+    }
+  }
+}
+
+.center {
+  text-align: center;
+}
+
+.rigth {
+  text-align: right;
+}
+</style>
