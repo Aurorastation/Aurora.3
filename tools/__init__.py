9bf730572a1c088462dea3cee470c81010acbaad
Sindorman <mikebykhovtsev@gmail.com>
2020-02-04 16:44:20 -0800

    These were not supposed to get in

--- tools/__init__.py
+++
@@ -1,1 +0,0 @@
-from .. import python_version
--- tools/python_version.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def check_version():
-    version = (3, 4)
-
-    if sys.version_info < version:
-        print("Sorry, this requires python >= {}. Your version is {}!".format(version, tuple(sys.version_info)))
-        exit()
