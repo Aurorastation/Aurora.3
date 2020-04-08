cf8bea8fea7d31744895c692e4abd8256beaaeb7
Sindorman <mikebykhovtsev@gmail.com>
2020-04-08 15:03:43 -0700

    wrong version

--- tools/python_version_check.py
+++ tools/python_version_check.py
@@ -1,6 +1,6 @@
 import sys
 
-version = (3, 9) # Check if python version is greater that this.
+version = (3, 4) # Check if python version is greater that this.
 
 if sys.version_info < version:
     print("Sorry, this requires python >= {}. Your version is {}!".format(version, tuple(sys.version_info)))
