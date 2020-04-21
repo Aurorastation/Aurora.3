a44ad77a61a196ce45f5b0573adc016c0ead192a
Geevies <ggrobler447@gmail.com>
2020-04-21 10:27:42 +0200

    better SQL

--- SQL/migrate/V046__IPC_tag_project.sql
+++ SQL/migrate/V046__IPC_tag_project.sql
@@ -6,10 +6,10 @@
 DROP TABLE `ss13_ipc_tracking`;
 
 CREATE TABLE `ss13_characters_ipc_tags` (
-  `char_id` int(11) unsigned NOT NULL,
+  `char_id` int(11) NOT NULL,
   `tag_status` tinyint(1) NOT NULL DEFAULT '0',
-  `serial_number` varchar(12) DEFAULT NULL,
-  `ownership_status` enum('Self Owned','Company Owned','Privately Owned') DEFAULT 'Company Owned',
+  `serial_number` varchar(12) DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
+  `ownership_status` enum('Self Owned','Company Owned','Privately Owned') DEFAULT 'Company Owned' COLLATE 'utf8mb4_unicode_ci',
   PRIMARY KEY (`char_id`),
-  CONSTRAINT `char_id` FOREIGN KEY (`char_id`) REFERENCES `ss13_characters` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4_unicode_ci;
+  CONSTRAINT `char_id` FOREIGN KEY (`char_id`) REFERENCES `ss13_characters` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB COLLATE='utf8mb4_unicode_ci'
