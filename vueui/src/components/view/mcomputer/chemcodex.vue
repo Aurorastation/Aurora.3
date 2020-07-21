7185c2c81d82656f942b770ae128a373107586c3
Karolis2011 <Karolis2011@users.noreply.github.com>
2020-07-21 09:16:14 +0300

    Adds toggle button and style from geeves

--- vueui/src/components/view/mcomputer/chemcodex.vue
+++ vueui/src/components/view/mcomputer/chemcodex.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <vui-input-search :input="s.reactions" v-model="output" :keys="['result.name', 'reagents.name', 'result.description']" autofocus :threshold="threshold" include-score/>
-    <div clsss="recipe-block" v-for="reac in output" :key="reac.item.id" :style="{opacity: 1 - (reac.score * score_multiplier)}">
+    <vui-button @click="desc = !desc">Toggle descriptions</vui-button>
+    <div class="recipe-block" v-for="reac in output" :key="reac.item.id" :style="{opacity: 1 - (reac.score * score_multiplier)}">
       <h2 class="highlight">{{ reac.item.result.name }} ({{ u(reac.item.result.amount) }})</h2>
-      <p>{{ reac.item.result.description }}</p>
+      <p v-show="desc">{{ reac.item.result.description }}</p>
       <h3 class="highlight">Required Reagents</h3>
       <span v-for="r in reac.item.reagents" :key="'re'+r.name+reac.item.result.name">{{ r.name }}: {{ u(r.amount) }}<br></span>
       <template v-if="reac.item.catalysts.length">
@@ -22,7 +23,6 @@
         <h3 class="highlight">Maximum required temparatures</h3>
         <span v-for="t in reac.item.temp_max" :key="'tx'+t.name+reac.item.result.name">{{ t.name }}: {{ t.temp }}&deg;K<br></span>
       </template>
-      <hr>
     </div>
   </div>
 </template>
@@ -33,7 +33,8 @@ export default {
     return {
       s: this.$root.$data.state,
       output: [],
-      threshold: 0.3
+      threshold: 0.3,
+      desc: true
     }
   },
   methods: {
@@ -54,4 +55,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.recipe-block {
+  h2, h3 {
+    padding: 2px 0;
+  }
+
+  background-color: rgba(0, 0, 0, 0);
+  border-bottom: 2px solid transparent;
+  transition: background-color 0.2s, color 0.2s border-bottom 0.2s;
+  padding: 2px;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.5);
+    border-bottom: 2px solid #4f7c9e;
+    }
+}
 </style>
