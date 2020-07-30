19a7aafadb3cc91827b74b0507a1c05d42373ced
Erki <erki.meinberg@gmail.com>
2020-07-30 19:21:30 +0300

    waagh

--- vueui/src/components/view/admin/permissions-panel.vue
+++ vueui/src/components/view/admin/permissions-panel.vue
@@ -3,25 +3,25 @@
     <div v-if="forumuserui_enabled" class="notice">
       <center>Modifications done do not outlast the round!</center>
     </div>
-    <div class="table">
-      <div class="header">
-        <div class="header-item">Ckey <vui-button :params="{ action: 'add' }">Add</vui-button></div>
-        <div class="header-item">Rank</div>
-        <div class="header-item">Permissions</div>
-      </div>
+    <table>
+      <tr>
+        <th>Ckey <vui-button :params="{ action: 'add' }">Add</vui-button></th>
+        <th>Rank</th>
+        <th>Permissions</th>
+      </tr>
 
-      <div class="entry" v-for="admin in admins" :key="admin.ckey">
-        <div class="item">
+      <tr v-for="admin in admins" :key="admin.ckey">
+        <td>
           <vui-button :params="{ action: 'remove', ckey: admin.ckey }">Edit</vui-button> {{admin.ckey}}
-        </div>
-        <div class="item">
+        </td>
+        <td>
           <vui-button :params="{ action: 'rank', ckey: admin.ckey }">Edit</vui-button> {{admin.rank}}
-        </div>
-        <div class="item" width="30%">
+        </td>
+        <td>
           <vui-button :params="{ action: 'rights', ckey: admin.ckey }">Edit</vui-button> {{admin.rights}}
-        </div>
-      </div>
-    </div>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -32,22 +32,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.table {
-  display: table;
-  width: 100%;
-
-  .header {
-    text-align: center;
-    font-weight: bold;
-  }
-  .header, .entry {
-    display: table-row;
-    .header-item, .item {
-      display: table-cell;
-      padding: 1px;
-    }
-  }
-}
-</style>
