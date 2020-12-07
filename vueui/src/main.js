14c4d9e8c2baf1bb6d10af9411b5c3d1bbc7e6fb
John Wildkins <john.wildkins@gmail.com>
2020-12-07 17:51:10 -0500

    Replace VueUI Titlebar backend with Karolis' version, fix window offset bugs

--- vueui/src/main.js
+++ vueui/src/main.js
@@ -8,7 +8,7 @@ import camelCase from 'lodash/camelCase'
 
 import Store from './store.js'
 import './assets/global.scss'
-import {setWindowKey, recallWindowGeometry} from './drag.js';
+import ByWin from './byWin'
 
 const requireComponent = require.context(
   './components', // The relative path of the components folder
@@ -40,11 +40,14 @@ if (document.getElementById("app")) {
   var state = JSON.parse(document.getElementById('initialstate').innerHTML)
 
   Store.loadState(state)
-  
+
   window.__wtimetimer = window.setInterval(() => {
     Store.state.wtime += 2
   }, 200)
-  
+
+  ByWin.setWindowKey(window.document.getElementById('vueui:windowId').getAttribute('content'))
+  ByWin.setupDrag()
+
   new Vue({
     data: Store.state,
     template: "<div id='content' tabindex='-1'><p class='csserror'>Javascript loaded, stylesheets has failed to load. <a href='javascript:void(0)'><vui-button :params='{ vueuiforceresource: 1}'>Click here to load.</vui-button></a></p><component v-if='componentName' :is='componentName'/><component v-if='templateString' :is='{template:templateString}'/></div>",
@@ -70,19 +73,6 @@ if (document.getElementById("app")) {
         deep: true
       }
     },
-    mounted: function () {
-      this.$nextTick(function() {
-        const options = {
-          size: [400, 600],
-        };
-        if (window.innerHeight && window.innerWidth) {
-          options.size = [window.innerWidth, window.innerHeight];
-        }
-        setWindowKey(document.getElementById('vueui:windowId').getAttribute('content'));
-        recallWindowGeometry(options);
-        document.getElementById('content').focus();
-      })
-    }
   }).$mount('#app')
 }
 
@@ -95,7 +85,7 @@ if (document.getElementById("header")) {
 if (document.getElementById("dapp")) {
   new Vue({
     data: Store.state,
-    template: '<div id="debug"><h2>Debug this UI with inspector by opening URL in your browser:</h2><pre>{{url}}</pre><h2>Current data of UI:</h2><pre>{{ JSON.stringify(this.$root.$data, null, \'    \') }}</pre><button @click="stop()">STOP WTIME TRACKING</button></div>',
+    template: '<div id="debug" :class="shouldShow"><h2>Debug this UI with inspector by opening URL in your browser:</h2><pre>{{url}}</pre><h2>Current data of UI:</h2><pre>{{ JSON.stringify(this.$root.$data, null, \'    \') }}</pre><button @click="stop()">STOP WTIME TRACKING</button></div>',
     methods: {
       stop() {
         window.clearInterval(window.__wtimetimer)
@@ -103,7 +93,10 @@ if (document.getElementById("dapp")) {
     },
     computed: {
       url() {
-        return window.location.href
+        return window.location.href + '?ext'
+      },
+      shouldShow() {
+        return this.$root.$data.debug_view ? "uiDebug" : ""
       }
     }
   }).$mount('#dapp')
