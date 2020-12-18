a623331cae095d2a24f72e047db05bf1300fed1f
Karolis2011 <Karolis2011@users.noreply.github.com>
2020-12-18 13:23:41 +0200

    Forgot a thing

--- vueui/src/components/view/console/atmocontrol/main.vue
+++ vueui/src/components/view/console/atmocontrol/main.vue
@@ -5,15 +5,15 @@
       <vui-group-row v-if="state.sensors.length <= 0">No sensors connected.</vui-group-row>
       <template v-else v-for="(sdata, key) in state.sensors">
         <vui-group-row :key="key"><b>{{ sdata.name }}</b></vui-group-row>
-        <vui-group :key="key" v-if="sdata.pressure" label="Pressure:">{{ sdata.pressure }} kPa</vui-group>
-        <vui-group :key="key" v-if="sdata.temperature" label="Temperature:">{{ sdata.temperature }} K</vui-group>
-        <vui-group :key="key" v-if="sdata.oxygen || sdata.hydrogen || sdata.phoron || sdata.nitrogen || sdata.carbon_dioxide" label="Gas Composition:">
+        <vui-group-item :key="key" v-if="sdata.pressure" label="Pressure:">{{ sdata.pressure }} kPa</vui-group-item>
+        <vui-group-item :key="key" v-if="sdata.temperature" label="Temperature:">{{ sdata.temperature }} K</vui-group-item>
+        <vui-group-item :key="key" v-if="sdata.oxygen || sdata.hydrogen || sdata.phoron || sdata.nitrogen || sdata.carbon_dioxide" label="Gas Composition:">
           <span class="complist" v-if="sdata.oxygen">{{ sdata.oxygen }} O<sub>2</sub></span>
           <span class="complist" v-if="sdata.nitrogen">{{ sdata.nitrogen }} N</span>
           <span class="complist" v-if="sdata.carbon_dioxide">{{ sdata.carbon_dioxide }} CO<sub>2<sub/></sub></span>
           <span class="complist" v-if="sdata.phoron">{{ sdata.phoron }} PH</span>
           <span class="complist" v-if="sdata.hydrogen">{{ sdata.hydrogen }} H<sub>2</sub></span>
-        </vui-group>
+        </vui-group-item>
       </template>
     </vui-group>
     <component v-if="state.control" :is="&quot;view-console-atmocontrol-&quot; + state.control"/>
