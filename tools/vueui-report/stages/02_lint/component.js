ebc1799b6e9e7bdc6f604573447d9c5407d8eed9
Karolis2011 <Karolis2011@users.noreply.github.com>
2020-12-21 16:27:47 +0200

    Nice output

--- tools/vueui-report/stages/02_lint/component.js
+++ tools/vueui-report/stages/02_lint/component.js
@@ -1,8 +1,21 @@
+var Convert = require('ansi-to-html');
+var convert = new Convert();
 module.exports = function (state) {
   return ({
-    template: '<div>{{ $data }}</div>',
+    template: `<div>
+      <pre v-html="lintOut"></pre>
+    </div>`,
     data() {
       return state
+    },
+    computed: {
+      lintOut() {
+        if(this.lint)
+        {
+          return convert.toHtml(this.lint)
+        }
+        return ''
+      }
     }
   })
 }
