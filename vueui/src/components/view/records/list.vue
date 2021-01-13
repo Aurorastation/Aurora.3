cba193b04da0573c58b387ceafd0d8612bc44d62
Geeves <ggrobler447@gmail.com>
2021-01-13 21:27:33 +0200

    what can go wrong
    Co-authored-by: Karolis <Karolis2011@users.noreply.github.com>

--- vueui/src/components/view/records/list.vue
+++ vueui/src/components/view/records/list.vue
@@ -2,12 +2,7 @@
   <div>
     <vui-input-search style="display: block;" :input="records" v-model="filtered" :keys="['id', 'name', 'rank', 'sex', 'age', 'fingerprint', 'blood', 'dna']"/>
     <div v-for="record in filtered" :key="record.id">
-      <div v-if="record.has_notes">
-        <vui-button :params="{ setactive: record.id }" icon="align-justify" @click="state.activeview = state.defaultview" push-state>{{ record.id }}: {{ record.name }} ({{ record.rank }})</vui-button>
-      </div>
-      <div v-else>
-        <vui-button :params="{ setactive: record.id }" @click="state.activeview = state.defaultview" push-state>{{ record.id }}: {{ record.name }} ({{ record.rank }})</vui-button>
-      </div>
+        <vui-button :params="{ setactive: record.id }" :icon="record.has_notes ? 'align-justify' : null" @click="state.activeview = state.defaultview" push-state>{{ record.id }}: {{ record.name }} ({{ record.rank }})</vui-button>
     </div>
     <vui-button class="newrecord" :v-if="(editable & 1) > 0" :params="{ newrecord: 1 }" @click="state.activeview = state.defaultview" push-state>New Record</vui-button>
   </div>
