06dc129749d8fff019e0f30026375d913f4143c1
AuroraBuildBot <buildbot@aurorastation.org>
2021-01-25 19:12:46 +0000

    "[ci skip] Automatic Build - 2021.01.25.19.12.46"

--- vueui/src/components/view/records/list.vue
+++ vueui/src/components/view/records/list.vue
@@ -2,7 +2,7 @@
   <div>
     <vui-input-search style="display: block;" :input="records" v-model="filtered" :keys="['id', 'name', 'rank', 'sex', 'age', 'fingerprint', 'blood', 'dna']"/>
     <div v-for="record in filtered" :key="record.id">
-        <vui-button :params="{ setactive: record.id }" :icon="record.has_notes ? 'align-justify' : null" @click="state.activeview = state.defaultview" push-state>{{ record.id }}: {{ record.name }} ({{ record.rank }})</vui-button>
+      <vui-button :params="{ setactive: record.id }" :icon="record.has_notes ? 'align-justify' : null" @click="state.activeview = state.defaultview" push-state>{{ record.id }}: {{ record.name }} ({{ record.rank }})</vui-button>
     </div>
     <vui-button class="newrecord" :v-if="(editable & 1) > 0" :params="{ newrecord: 1 }" @click="state.activeview = state.defaultview" push-state>New Record</vui-button>
   </div>
