2fe0ef74f553d90d280b48425db13c2ff32eaf71
MarinaGryphon <ryfoxx93@gmail.com>
2021-05-05 14:26:04 -0500

    dependencies are already in the docker build

--- scripts/install-byond.sh
+++ scripts/install-byond.sh
@@ -4,10 +4,6 @@ if [ -f "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/DreamMaker" ];
 then
   echo "Using cached BYOND directory."
 else
-  echo "Installing dependencies."
-  sudo dpkg --add-architecture i386
-  sudo apt update
-  sudo apt install libstdc++6:i386 gcc-multilib g++-7 g++-7-multilib libssl1.1:i386 zlib1g:i386
   echo "Setting up BYOND."
   mkdir -p "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}"
   cd "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}"
