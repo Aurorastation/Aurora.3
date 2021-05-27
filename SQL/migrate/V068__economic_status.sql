1773bd83ecfce9b93d744b3f0cde6081a80e30da
TheGreyWolf <mc-casper@hotmail.dk>
2021-05-27 02:52:01 +0200

    SQL migration

---
+++ SQL/migrate/V068__economic_status.sql
@@ -0,0 +1,8 @@
+--
+-- Implemented in PR #11334.
+-- Renames NanoTrasen Relation into Economic Status.
+--
+
+ALTER TABLE `ss13_characters`
+	DROP COLUMN `nt_relation`,
+	ADD COLUMN `economic_status` MEDIUMTEXT DEFAULT "Average" AFTER `religion`;
