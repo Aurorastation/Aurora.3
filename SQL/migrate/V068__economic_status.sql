980bc70e680a7495287b8a7254138a5afe579c60
TheGreyWolf <mc-casper@hotmail.dk>
2021-05-27 15:04:46 +0200

    changelog & final migration change

--- SQL/migrate/V068__economic_status.sql
+++ SQL/migrate/V068__economic_status.sql
@@ -1,8 +1,8 @@
 --
--- Implemented in PR #11334.
+-- Implemented in PR #11967.
 -- Renames NanoTrasen Relation into Economic Status.
 --
 
 ALTER TABLE `ss13_characters`
 	DROP COLUMN `nt_relation`,
-	ADD COLUMN `economic_status` MEDIUMTEXT DEFAULT "Average" AFTER `religion`;
+	ADD COLUMN `economic_status` ENUM('Wealthy', 'Well-off', 'Average', 'Underpaid', 'Poor') DEFAULT "Average" AFTER `religion`;
