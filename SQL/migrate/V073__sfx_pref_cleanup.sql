4f1d4d920f8430fcc0b50a288bf58ee33564fa40
SleepyGemmy <99297919+SleepyGemmy@users.noreply.github.com>
2022-08-26 16:38:25 +0200

    Suggested Tweak

--- SQL/migrate/V073__sfx_pref_cleanup.sql
+++ SQL/migrate/V073__sfx_pref_cleanup.sql
@@ -4,5 +4,5 @@
 -- Rename "asfx_togs" to "sfx_toggles".
 -- 
 
-UPDATE `ss13_player_preferences` SET `toggles` = `toggles` ~| 0x4;
+UPDATE `ss13_player_preferences` SET `toggles` = `toggles` & ~0x4;
 ALTER TABLE `ss13_player_preferences` CHANGE COLUMN `asfx_togs` `sfx_toggles` INT(11) NULL DEFAULT '0' AFTER `UI_style_alpha`;
