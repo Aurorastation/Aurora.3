6d89fa9df40de6877826304af8a87012842d1e49
FlamingLily <realFlamingLily@gmail.com>
2023-03-22 01:07:04 +1100

    all of my issues with SQL have been incredibly stupid and simple things like names and data types

--- SQL/migrate/V078__char_heights.sql
+++ SQL/migrate/V078__char_heights.sql
@@ -2,4 +2,4 @@
 -- Adds character heights
 --
 
-ALTER TABLE `ss13_characters` ADD COLUMN `character_height` INT(3) NOT NULL DEFAULT 0 AFTER `species`
+ALTER TABLE `ss13_characters` ADD COLUMN `char_height` INT(3) NOT NULL DEFAULT 0 AFTER `species`
