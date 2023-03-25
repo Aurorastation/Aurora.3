7b33854a1c2672409e8675edf1eb1d4893249256
Fluffy <65877598+FluffyGhoster@users.noreply.github.com>
2023-03-25 09:29:36 +0100

    dafsdfa

--- tools/pratomizer/pratomizer.py
+++ tools/pratomizer/pratomizer.py
@@ -6,7 +6,7 @@ from git import SymbolicReference
 @click.option('--file')
 @click.option('--prname')
 @click.option('--commitmessage', default='Atomization')
-def hello(file, prname):
+def hello(file, prname, commitmessage):
 
     filecontent = 0
 
@@ -17,29 +17,38 @@ def hello(file, prname):
             print(f'Unable to open, find or read the file {file}')
             exit()
 
-    GitMakeRepo(prname)
+    repo = GitMakeRepo(prname)
     CopyFileInNewRepo(file, filecontent)
+    repo: Repo = repo
+    CommitAtomization(repo, commitmessage)
 
 
 
 def GitMakeRepo(name):
-    repo = Repo("..\\..\\")
-    if repo.is_dirty(untracked_files=True):
-        print("There are uncommitted changes in the current branch.")
-        print("Please commit or stash your changes before creating a new branch.")
-    else:
-        try:
-            repo.git.checkout("master")
-            new_branch: 'SymbolicReference' = repo.create_head(f'{name}')
-            new_branch.checkout()
-            repo.git.checkout(name)
-            return new_branch
-        except():
-            print('Something went wrong in the cloning of the branch')
+    try:
+        repo = Repo("..\\..\\")
+        if repo.is_dirty(untracked_files=True):
+            print("There are uncommitted changes in the current branch.")
+            print("Please commit or stash your changes before creating a new branch.")
+        else:
+            try:
+                repo.git.checkout("master")
+                new_branch: 'SymbolicReference' = repo.create_head(f'{name}')
+                new_branch.checkout()
+                repo.git.checkout(name)
+                return repo
+            except():
+                print('Something went wrong in the cloning of the branch')
+    except():
+        print("Unable to acquire repo.")
 
 def CopyFileInNewRepo(filepath, content):
     with open(filepath, 'wb') as f:
         f.write(content)
+
+def CommitAtomization(repo: Repo, message: String):
+    repo.index.add("*")
+    repo.index.commit(message)
 
 
 if __name__ == '__main__':
