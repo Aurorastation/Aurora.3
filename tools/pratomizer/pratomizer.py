c5c7f62f161e7aa8a51418d8052d32c8079c9899
Fluffy <65877598+FluffyGhoster@users.noreply.github.com>
2023-03-25 09:16:01 +0100

    L

--- tools/pratomizer/pratomizer.py
+++ tools/pratomizer/pratomizer.py
@@ -1,5 +1,6 @@
 import click
 from git import Repo
+from git import SymbolicReference
 
 @click.command()
 @click.option('--file')
@@ -8,31 +9,38 @@ def hello(file, prname):
 
     filecontent = 0
 
-    with open(file, 'r', encoding='utf-8') as f:
+    with open(file, 'rb', encoding='utf-8') as f:
         try:
-          filecontent = f.read()
+            filecontent = f.read()
         except():
             print(f'Unable to open, find or read the file {file}')
             exit()
-    main(filecontent)
-    GitMakeRepo(prname)
 
-def main(filecontent):
-   print(filecontent)
+    GitMakeRepo(prname)
+    CopyFileInNewRepo(file, filecontent)
+
 
 
 def GitMakeRepo(name):
     repo = Repo("..\\..\\")
     current_branch = repo.active_branch
     if repo.is_dirty(untracked_files=True):
-      print("There are uncommitted changes in the current branch.")
-      print("Please commit or stash your changes before creating a new branch.")
+        print("There are uncommitted changes in the current branch.")
+        print("Please commit or stash your changes before creating a new branch.")
     else:
-       repo.git.checkout("master")
-       new_branch = repo.create_head(f'{name}')
-       new_branch.checkout()
+        try:
+          repo.git.checkout("master")
+          new_branch: 'SymbolicReference' = repo.create_head(f'{name}')
+          new_branch.checkout()
+          repo.git.checkout(name)
+          return new_branch
+        except():
+            print('Something went wrong in the cloning of the branch')
 
-    repo.git.checkout(name)
+def CopyFileInNewRepo(filepath, content):
+    with open(filepath, 'wb', encoding='utf-8') as f:
+        f.write(content)
+
 
 if __name__ == '__main__':
     exit(hello())
