c6fa0d7dd7dc219942168b94c120149ecb94a1fb
Matt Atlas <liermattia@gmail.com>
2023-05-30 13:21:03 +0200

    multitool UI

--- vueui/src/components/view/devices/multitool.vue
+++ vueui/src/components/view/devices/multitool.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3>Smart Track</h3>
-    <vui-button :disabled="has_selected_io == true" :class="{'button': 1, 'selected': tracking_apc}" :params="{track_apc: 1}">{{ has_selected_io ? "Toggle APC Smart Tracking (I/O in Buffer)" : "Toggle APC Smart Tracking" }}</vui-button> <vui-tooltip label="?">Enabling this will update the multitool's icon to point in the direction of the nearest APC <i>within your area</i>.</vui-tooltip>
+    <vui-button :dis:params="{track_apc: 1}">{{ has_selected_io ? "Toggle APC Smart Tracking (I/O in Buffer)" : "Toggle APC Smart Tracking" }}</vui-button> <vui-tooltip label="?">Enabling this will update the multitool's icon to point in the direction of the nearest APC <i>within your area</i>.</vui-tooltip>
     <h3>Circuit Input/Output</h3>
     <vui-group>
       <vui-group-item label="I/O Name:">{{ selected_io_name }}</vui-group-item>
