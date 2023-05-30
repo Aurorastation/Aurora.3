9e5d2b7652299be72e21a2b061c779d87d1abfd4
Matt Atlas <liermattia@gmail.com>
2023-05-30 12:51:29 +0200

    fuel injector

--- tgui/packages/tgui/interfaces/AtmosControlInjector.tsx
+++ tgui/packages/tgui/interfaces/AtmosControlInjector.tsx
@@ -1,6 +1,6 @@
 import { BooleanLike } from '../../common/react';
 import { useBackend } from '../backend';
-import { Section, LabeledList } from '../components';
+import { Section, LabeledList, Button } from '../components';
 import { Window } from '../layouts';
 import { AtmosControl } from './AtmosControl';
 
@@ -12,7 +12,7 @@ type Device = {
   power: BooleanLike;
   rate: number;
   automation: BooleanLike;
-}
+};
 
 export const AtmosControlInjector = (props, context) => {
   const { act, data } = useBackend<InjectorData>(context);
@@ -23,16 +23,57 @@ export const AtmosControlInjector = (props, context) => {
         <Section>
           <AtmosControl />
         </Section>
-        <Section title="Fuel Injection System">
-          <LabeledList>
-            <LabeledList.Item label="Input">
-              <Button
-                content={data.}
-              />
-            </LabeledList.Item>
-          </LabeledList>
-        </Section>
+        {data.device ? (
+          <InjectorWindow />
+        ) : (
+          <Button
+            content="Search Device"
+            onClick={() => act('in_refresh_status')}
+          />
+        )}
       </Window.Content>
     </Window>
+  );
+};
+
+export const InjectorWindow = (props, context) => {
+  const { act, data } = useBackend<InjectorData>(context);
+
+  return (
+    <Section title="Fuel Injection System">
+      <LabeledList>
+        <LabeledList.Item label="Input">
+          <Button
+            content={data.device.power ? 'Injecting' : 'On Hold'}
+            color={data.device.power ? 'good' : ''}
+            icon="power-off"
+            onClick={() => act('in_toggle_injector')}
+          />
+        </LabeledList.Item>
+        <LabeledList.Item label="Rate">{data.device.rate} L/s</LabeledList.Item>
+        <LabeledList.Item label="Automated Injection">
+          <Button
+            content={data.device.automation ? 'Engaged' : 'Disengaged'}
+            color={data.device.automation ? 'good' : ''}
+            icon="calendar"
+            onClick={() => act('toggle_automation')}
+          />
+        </LabeledList.Item>
+        <LabeledList.Item label="Injection">
+          <Button
+            content="Toggle Power"
+            icon="power-off"
+            disabled={data.device.automation}
+            onClick={() => act('toggle_injector')}
+          />
+          <Button
+            content="Inject (1 Cycle)"
+            icon="refresh"
+            disabled={data.device.automation}
+            onClick={() => act('toggle_injector')}
+          />
+        </LabeledList.Item>
+      </LabeledList>
+    </Section>
   );
 };
