971a47362198477345506d22c1549a2e306eabae
Fluffy <65877598+FluffyGhoster@users.noreply.github.com>
2023-06-12 17:37:39 +0200

    Try reintroduction of tag-matcher

--- tools/ci/setup_od.sh
+++ tools/ci/setup_od.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -eo pipefail
 
-git clone https://github.com/OpenDreamProject/OpenDream.git OpenDream
-cd OpenDream
+git clone https://github.com/OpenDreamProject/OpenDream.git ../OpenDream
+cd ../OpenDream
 git submodule update --init --recursive
 dotnet restore
 dotnet build -c Release
-cd ..
