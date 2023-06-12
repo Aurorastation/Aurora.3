b6f2cf073264a680db2f03ce50d594c65312b308
Fluffy <65877598+FluffyGhoster@users.noreply.github.com>
2023-06-12 18:30:03 +0200

    Hate

--- tools/ci/setup_od.sh
+++ tools/ci/setup_od.sh
@@ -3,5 +3,5 @@ set -eo pipefail
 
 git clone https://github.com/OpenDreamProject/OpenDream.git ../OpenDream
 git -C ../OpenDream submodule update --init --recursive
-dotnet restore
+dotnet restore ../OpenDream
 dotnet build ../OpenDream/OpenDream.sln -c Release --property WarningLevel=1
