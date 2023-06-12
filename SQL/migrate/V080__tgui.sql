d63d2c75e888b23610fcd0e1342cdd9624b050de
Matt Atlas <liermattia@gmail.com>
2023-06-12 19:43:43 +0200

    account database

--- SQL/migrate/V080__tgui.sql
+++ SQL/migrate/V080__tgui.sql
@@ -2,5 +2,5 @@
 -- Adds tgui prefs
 --
 
-ALTER TABLE `ss13_player_preferences` ADD COLUMN `tgui_fancy` INT(1) NOT NULL DEFAULT 1 AFTER `html_UI_style`
-ALTER TABLE `ss13_player_preferences` ADD COLUMN `tgui_lock` INT(1) NOT NULL DEFAULT 0 AFTER `tgui_fancy`
+ALTER TABLE `ss13_player_preferences` ADD COLUMN `tgui_fancy` INT(1) NOT NULL DEFAULT 1 AFTER `html_UI_style`;
+ALTER TABLE `ss13_player_preferences` ADD COLUMN `tgui_lock` INT(1) NOT NULL DEFAULT 0 AFTER `tgui_fancy`;
