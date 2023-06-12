816af5023dc5fd23b5847a6a556ad49759f50ed5
Fluffy <65877598+FluffyGhoster@users.noreply.github.com>
2023-06-12 10:06:01 +0200

    dsf

--- tools/ci/run_od.sh
+++ tools/ci/run_od.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -eo pipefail
-dotnet OpenDream/OpenDream/DMCompiler/bin/Release/net7.0/DMCompiler.dll --suppress-unimplemented paradise.dme
+dotnet OpenDream/OpenDream/DMCompiler/bin/Release/net7.0/DMCompiler.dll --suppress-unimplemented aurorastation.dme
