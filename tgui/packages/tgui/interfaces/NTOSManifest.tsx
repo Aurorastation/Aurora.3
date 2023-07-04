ef283481956181097ce698c3564527e7e130ca06
Matt Atlas <mattiathebest2000@hotmail.it>
2023-07-04 21:04:38 +0200

    Actually fixes the manifest (#16634)
    * fix the manifest
    
    * cl
    
    * if your voices will feel the sky
    
    * OOPS
    
    * Update html/changelogs/mattatlas-manifix.yml
    
    ---------
    
    Co-authored-by: Matt Atlas <liermattia@gmail.com>

--- tgui/packages/tgui/interfaces/NTOSManifest.tsx
+++ tgui/packages/tgui/interfaces/NTOSManifest.tsx
@@ -1,11 +1,11 @@
 import { NtosWindow } from '../layouts';
-import { Manifest } from './common/Manifest';
+import { ManifestSection } from './common/ManifestSection';
 
 export const NTOSManifest = () => {
   return (
     <NtosWindow width={500} height={700}>
       <NtosWindow.Content scrollable>
-        <Manifest />
+        <ManifestSection />
       </NtosWindow.Content>
     </NtosWindow>
   );
