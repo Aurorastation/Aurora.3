cc4e5d395e230f0fe9a99a99a82648e0258b6e45
DreamySkrell <>
2023-07-20 11:29:18 +0200

    stuff copied from the other branch

---
+++ tgui/packages/tgui/interfaces/Docks.tsx
@@ -0,0 +1,60 @@
+import { useBackend } from '../backend';
+import { Button, LabeledList, Section, Table } from '../components';
+import { NtosWindow } from '../layouts';
+
+export type DocksData = {
+  docks: Dock[];
+};
+
+type Dock = {
+  name: string;
+  shuttle: string;
+};
+
+function sortByNameFn(a: Dock, b: Dock): number {
+  if (a.name < b.name){
+    return -1;
+  }
+  if (a.name > b.name){
+    return 1;
+  }
+  return 0;
+}
+
+export const Docks = (props, context) => {
+  const { act, data } = useBackend<DocksData>(context);
+  const full_docks = data.docks.filter((d: Dock) => !!d.shuttle).sort(sortByNameFn);
+  const empty_docks = data.docks.filter((d: Dock) => !d.shuttle).sort(sortByNameFn);
+
+  return (
+    <NtosWindow resizable>
+      <NtosWindow.Content scrollable>
+        <Section title="Docking Ports Management Program">
+          <Table>
+            <Table.Row header>
+              <Table.Cell>Port/Hangar</Table.Cell>
+              <Table.Cell>Docked Craft</Table.Cell>
+            </Table.Row>
+            <Table.Row></Table.Row>
+            {full_docks.map((dock) => (
+              <Table.Row>
+                <Table.Cell>{dock.name}</Table.Cell>
+                <Table.Cell>
+                  {dock.shuttle}
+                </Table.Cell>
+              </Table.Row>
+            ))}
+            {empty_docks.map((dock) => (
+              <Table.Row color="gray">
+                <Table.Cell>{dock.name}</Table.Cell>
+                <Table.Cell>
+                  {dock.shuttle}
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table>
+        </Section>
+      </NtosWindow.Content>
+    </NtosWindow>
+  );
+};
