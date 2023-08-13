460e9d1e3fa3899dc10f914a3ec554be902d18a6
Fluffy <65877598+FluffyGhoster@users.noreply.github.com>
2023-08-13 18:11:16 +0200

    1613

--- dependencies.sh
+++ dependencies.sh
@@ -4,7 +4,7 @@ if [ -z ${GITHUB_ENV+x} ]; then GITHUB_ENV=/dev/null; fi
 
 export BYOND_MAJOR=515
 echo "BYOND_MAJOR=$BYOND_MAJOR" >> "$GITHUB_ENV"
-export BYOND_MINOR=1611
+export BYOND_MINOR=1613
 echo "BYOND_MINOR=$BYOND_MINOR" >> "$GITHUB_ENV"
 
 export RUST_G_VERSION=v1.2.0+a4
