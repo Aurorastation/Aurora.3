0a86b1616917667313420c28772422d3a5e5b48a
HanSolo1519 <72716882+HanSolo1519@users.noreply.github.com>
2023-09-08 12:43:10 -0400

    Delete SQL/migrate/V002__ruined_economic_status.sql

--- SQL/migrate/V002__ruined_economic_status.sql
+++
@@ -1,5 +0,0 @@
---
--- From PR #17226
--- Adds the ruined economic status to the database.
---
-ALTER TABLE `ss13_characters` MODIFY COLUMN `economic_status` ENUM('Wealthy', 'Well-off', 'Average', 'Underpaid', 'Poor', 'Impoverished', 'Ruined');
