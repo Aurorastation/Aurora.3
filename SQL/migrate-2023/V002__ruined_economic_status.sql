285449526e5ee85481d1d5ef99f0af0d226bf433
SleepyGemmy <99297919+SleepyGemmy@users.noreply.github.com>
2023-09-08 22:57:40 +0200

    Fix PR Number

--- SQL/migrate-2023/V002__ruined_economic_status.sql
+++ SQL/migrate-2023/V002__ruined_economic_status.sql
@@ -1,5 +1,5 @@
 --
--- From PR #17226
+-- Implemented in PR #17227.
 -- Adds the ruined economic status to the database.
 --
 ALTER TABLE `ss13_characters` MODIFY COLUMN `economic_status` ENUM('Wealthy', 'Well-off', 'Average', 'Underpaid', 'Poor', 'Impoverished', 'Ruined');
