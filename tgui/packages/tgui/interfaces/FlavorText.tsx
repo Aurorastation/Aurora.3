9608e6be5116ef28aff1c38219a1f694f2ea9579
Geeves <ggrobler447@gmail.com>
2023-09-17 15:04:45 +0200

    we love linetrs

--- tgui/packages/tgui/interfaces/FlavorText.tsx
+++ tgui/packages/tgui/interfaces/FlavorText.tsx
@@ -1,6 +1,5 @@
-import { BooleanLike } from '../../common/react';
-import { useBackend, useLocalState } from '../backend';
-import { Box, Button, Collapsible, Divider, Input, Section } from '../components';
+import { useBackend } from '../backend';
+import { Box, Divider, Section } from '../components';
 import { Window } from '../layouts';
 
 export type FlavorTextData = {
@@ -9,7 +8,7 @@ export type FlavorTextData = {
 
 // Credits to https://www.js-craft.io/blog/react-detect-url-text-convert-link
 // for this Linkify code
-const Linkify = ({ children }) => {
+const Linkify = ({ text }) => {
   const isUrl = (word) => {
     const urlPattern =
       /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm;
@@ -20,10 +19,10 @@ const Linkify = ({ children }) => {
     return isUrl(word) ? `<a href="${word}">${word}</a>` : word;
   };
 
-  const words = children.split(' ');
+  const words = text.split(' ');
   const formatedWords = words.map((w, i) => addMarkup(w));
   const html = formatedWords.join(' ');
-  return <Box key={children} dangerouslySetInnerHTML={{ __html: html }} />;
+  return <Box key={text} dangerouslySetInnerHTML={{ __html: html }} />;
 };
 
 export const FlavorText = (props, context) => {
@@ -36,7 +35,7 @@ export const FlavorText = (props, context) => {
           {data.flavor_text.split('\n').map((line) =>
             line ? (
               <Box>
-                <Linkify children={line}></Linkify>
+                <Linkify text={line} />
                 <Divider />
               </Box>
             ) : null
