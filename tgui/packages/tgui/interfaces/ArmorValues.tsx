91eff4aedd09850b9581c58ac8de5d879bb84160
Geeves <ggrobler447@gmail.com>
2023-09-17 14:48:42 +0200

    Examinable Armor

---
+++ tgui/packages/tgui/interfaces/ArmorValues.tsx
@@ -0,0 +1,39 @@
+import { BooleanLike } from '../../common/react';
+import { useBackend, useLocalState } from '../backend';
+import { Box, Button, Collapsible, Divider, Input, ProgressBar, Section } from '../components';
+import { Window } from '../layouts';
+
+export type ArmorValuesData = {
+  armor_values: string[];
+};
+
+export const ArmorValues = (props, context) => {
+  const { act, data } = useBackend<ArmorValuesData>(context);
+
+  return (
+    <Window resizable>
+      <Window.Content scrollable>
+        <Section>
+          {Object.keys(data.armor_values).map((line) =>
+            line ? (
+              <Box>
+                <Box pb={1}>{line.toUpperCase()}</Box>
+                <ProgressBar
+                  ranges={{
+                    good: [50, 100],
+                    average: [30, 50],
+                    bad: [0, 30],
+                  }}
+                  value={data.armor_values[line]}
+                  minValue={0}
+                  maxValue={100}
+                />
+                <Divider />
+              </Box>
+            ) : null
+          )}
+        </Section>
+      </Window.Content>
+    </Window>
+  );
+};
