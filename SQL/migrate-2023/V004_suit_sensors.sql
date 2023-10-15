d0aa032e91fa82565538b575d6b6fdc4bab67f09
Wezzy <wowzewow@gmail.com>
2023-10-16 00:01:21 +0800

    damn me dumb

--- SQL/migrate-2023/V004_suit_sensors.sql
+++ SQL/migrate-2023/V004_suit_sensors.sql
@@ -2,5 +2,5 @@
 -- Implemented in PR #17607.
 -- Adds suit sensor prefs.
 --
-ALTER TABLE `ss13_player_preferences` ADD COLUMN `sensor_setting` INT(11) DEFAULT NULL
-ALTER TABLE `ss13_player_preferences` ADD COLUMN `sensors_locked` INT(11) DEFAULT NULL
+ALTER TABLE `ss13_characters` ADD COLUMN `sensor_setting` INT(11) DEFAULT NULL
+ALTER TABLE `ss13_characters` ADD COLUMN `sensors_locked` INT(11) DEFAULT NULL
