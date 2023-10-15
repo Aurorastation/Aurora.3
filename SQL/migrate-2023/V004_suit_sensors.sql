42dea928499fd0d5f989f66c693be11656bef09d
Wezzy <wowzewow@gmail.com>
2023-10-15 23:23:11 +0800

    this will work hopefully

--- SQL/migrate-2023/V004_suit_sensors.sql
+++ SQL/migrate-2023/V004_suit_sensors.sql
@@ -2,5 +2,5 @@
 -- Implemented in PR #17607.
 -- Adds suit sensor prefs.
 --
-ALTER TABLE `ss13_player_preferences` ADD `sensor_setting` INT(11) DEFAULT NULL
-ALTER TABLE `ss13_player_preferences` ADD `sensors_locked` INT(11) DEFAULT NULL
+ALTER TABLE `ss13_player_preferences` ADD COLUMN `sensor_setting` INT(11) DEFAULT NULL
+ALTER TABLE `ss13_player_preferences` ADD COLUMN `sensors_locked` INT(11) DEFAULT NULL
