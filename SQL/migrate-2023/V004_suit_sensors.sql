c23868205604e10a8ff728e1cd77b87fcf06ce04
Fluffy <65877598+FluffyGhoster@users.noreply.github.com>
2023-10-15 21:30:42 +0200

    Apply suggestions from code review

--- SQL/migrate-2023/V004_suit_sensors.sql
+++ SQL/migrate-2023/V004_suit_sensors.sql
@@ -2,5 +2,5 @@
 -- Implemented in PR #17607.
 -- Adds suit sensor prefs.
 --
-ALTER TABLE `ss13_characters` ADD COLUMN `sensor_setting` INT(11) DEFAULT NULL AFTER `headset_choice`;
-ALTER TABLE `ss13_characters` ADD COLUMN `sensors_locked` INT(11) DEFAULT NULL AFTER `sensor_setting`;
+ALTER TABLE `ss13_characters` ADD `sensor_setting` INT(11) DEFAULT NULL AFTER `headset_choice`;
+ALTER TABLE `ss13_characters` ADD `sensors_locked` INT(11) DEFAULT NULL AFTER `sensor_setting`;
