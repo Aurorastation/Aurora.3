c9d86c2e2a197dccc12e166e62b88c8344ab4f6a
DreamySkrell <>
2023-11-06 19:47:05 +0100

    a

--- tgui/packages/tgui/interfaces/Map.tsx
+++ tgui/packages/tgui/interfaces/Map.tsx
@@ -1,6 +1,7 @@
 import { useBackend } from '../backend';
-import { Box, Section, Table } from '../components';
+import { Input, Box, Section, Table } from '../components';
 import { NtosWindow } from '../layouts';
+import { TextInputModal } from './TextInputModal';
 
 export type MapData = {
   map_images: any; // base64 icon
@@ -17,27 +18,31 @@ export const Map = (props, context) => {
       <NtosWindow.Content scrollable>
         <Section title="Map Program">
           <svg
-            height={510}
-            width={510}
-            viewBox="0 0 255 255"
+            height={500}
+            width={500}
+            viewBox="0 0 480 480"
             overflow={'hidden'}>
-            <rect width="255" height="255" />
-            <image
-              // x="-50"
-              // y="-50"
-              width={255}
-              height={255}
-              xlinkHref={`data:image/jpeg;base64,${
-                data.map_images[data.user_z - 1]
-              }`}
-            />
-            <polygon
-              points="2,0 0,2 -2,0 0,-2"
-              fill="#5c83b0"
-              stroke="white"
-              stroke-width="0.5"
-              transform={`translate(${data.user_x} ${data.user_y})`}
-            />
+            <rect width="480" height="480" />
+            <g
+            //transform="translate(-240 -240)"
+            >
+              <image
+                width={480}
+                height={480}
+                xlinkHref={`data:image/jpeg;base64,${
+                  data.map_images[data.user_z - 1]
+                }`}
+              />
+              <polygon
+                points="2,0 0,2 -2,0 0,-2"
+                fill="#FF0000"
+                stroke="white"
+                stroke-width="0.5"
+                transform={`translate(${data.user_x + (480 - 255) / 2} ${
+                  255 - data.user_y + (480 - 255) / 2
+                })`}
+              />
+            </g>
           </svg>
         </Section>
       </NtosWindow.Content>
