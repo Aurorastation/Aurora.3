00622906deccb1b2ed50d700a371b552899b34ae
Werner <Arrow768@users.noreply.github.com>
2023-12-30 17:00:24 +0100

    Death Stats Fix
    Changes how the coordinates for the death stats are stored.
    -> One column for x,y,z instead of all mashed into one column

---
+++ SQL/migrate-2023/V006__death_table_fix.sql
@@ -0,0 +1,7 @@
+--
+-- Adds a column for single coordinate instead of mashing them into one column
+--
+ALTER TABLE `ss13_death`
+	ADD COLUMN `loc_x` INT NULL AFTER `oxyloss`,
+	ADD COLUMN `loc_y` INT NULL AFTER `loc_x`,
+	ADD COLUMN `loc_z` INT NULL AFTER `loc_y`;
