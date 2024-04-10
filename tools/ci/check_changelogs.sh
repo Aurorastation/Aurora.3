c93e1b8c4457160ec4dcd8af7ddd747ee1ebe840
Fluffy <65877598+FluffyGhoster@users.noreply.github.com>
2024-04-10 18:44:53 +0200

    sdfa

--- tools/ci/check_changelogs.sh
+++ tools/ci/check_changelogs.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 echo "5ed5da1545365bd137f3d31168a69342 *html/changelogs/example.yml" | md5sum -c -
-python3 tools/GenerateChangelog/ss13_genchangelog.py html/changelog.html html/changelogs --dry-run
+python3 tools/GenerateChangelog/ss13_genchangelog.py html/changelogs
