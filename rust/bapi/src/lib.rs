68b48b78410f6d87971991174662fa7413f2fde2
DreamySkrell <>
2024-05-15 23:42:51 +0200

    t

--- rust/bapi/src/lib.rs
+++ rust/bapi/src/lib.rs
@@ -14,15 +14,16 @@ fn dm_call_stack_trace(msg: String) {
 }
 
 /// Panic handler, called on unhandled errors.
-/// Calls dm stack trace proc with panic info, and writes it out to a text file.
+/// Writes panic info to a text file, and calls dm stack trace proc as well.
 fn setup_panic_handler() {
     std::panic::set_hook(Box::new(|info| {
         let msg = format!("RUST BAPI PANIC \n {:#?}", info);
-        crate::dm_call_stack_trace(msg.clone());
-        std::fs::write("./bapi_panic.txt", msg).unwrap();
+        std::fs::write("./bapi_panic.txt", msg.clone());
+        crate::dm_call_stack_trace(msg);
     }))
 }
 
+/// Turns spacemandmm map object to string.
 pub fn map_to_string(map: &dmmtools::dmm::Map) -> eyre::Result<String> {
     let mut v = vec![];
     map.to_writer(&mut v)?;
@@ -30,6 +31,7 @@ pub fn map_to_string(map: &dmmtools::dmm::Map) -> eyre::Result<String> {
     Ok(s)
 }
 
+///
 #[no_mangle]
 pub unsafe extern "C" fn read_dmm_file_ffi(
     argc: byondapi::sys::u4c,
@@ -45,15 +47,22 @@ pub unsafe extern "C" fn read_dmm_file_ffi(
     }
 }
 
+///
 fn read_dmm_file(path: ByondValue) -> eyre::Result<ByondValue> {
     setup_panic_handler();
-    let path: String = path.get_string()?;
-    // if !path.ends_with(".dmm") {
-    //     return Ok(ByondValue::null());
-    // }
 
+    let path: String = path.get_string()?;
     let path: std::path::PathBuf = path.try_into()?;
+
+    // some checks, just return null if path is bad for whatever reason
+    if !path.is_file() || !path.exists() {
+        return Ok(ByondValue::null());
+    }
+
+    // read file and parse with spacemandmm
     let dmm = dmmtools::dmm::Map::from_file(&path)?;
+
+    // return the map converted to string
     let str = map_to_string(&dmm)?;
     Ok(ByondValue::new_str(str)?)
 }
