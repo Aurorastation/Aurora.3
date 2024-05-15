1da3533494b25d7897d7d9b1c49aa67e552b4c76
DreamySkrell <>
2024-05-15 22:48:12 +0200

    2

--- rust/bapi/src/lib.rs
+++ rust/bapi/src/lib.rs
@@ -5,8 +5,8 @@ fn dm_call_stack_trace(msg: String) {
     let msg = byondapi::value::ByondValue::try_from(msg).unwrap();
     byondapi::global_call::call_global_id(
         {
-            static STRING_ID: ::std::sync::OnceLock<u32> = ::std::sync::OnceLock::new();
-            *STRING_ID.get_or_init(|| ::byondapi::byond_string::str_id_of("_stack_trace").unwrap())
+            static STRING_ID: std::sync::OnceLock<u32> = std::sync::OnceLock::new();
+            *STRING_ID.get_or_init(|| byondapi::byond_string::str_id_of("_stack_trace").unwrap())
         },
         &[msg],
     )
@@ -23,47 +23,29 @@ fn setup_panic_handler() {
     }))
 }
 
-pub fn map_to_string(map: &dmmtools::dmm::Map) -> Option<String> {
+pub fn map_to_string(map: &dmmtools::dmm::Map) -> eyre::Result<String> {
     let mut v = vec![];
-    map.to_writer(&mut v).ok()?;
-    let s = String::from_utf8(v).ok()?;
-    Some(s)
+    map.to_writer(&mut v)?;
+    let s = String::from_utf8(v)?;
+    Ok(s)
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn read_dmm_file_ffi(
-    __argc: ::byondapi::sys::u4c,
-    __argv: *mut ::byondapi::value::ByondValue,
-) -> ::byondapi::value::ByondValue {
-    let args = unsafe { ::byondapi::parse_args(__argc, __argv) };
-    match read_dmm_file(
-        args.get(0usize)
-            .map(::byondapi::value::ByondValue::clone)
-            .unwrap_or_default(),
-    ) {
+    argc: byondapi::sys::u4c,
+    argv: *mut byondapi::value::ByondValue,
+) -> byondapi::value::ByondValue {
+    let args = unsafe { ::byondapi::parse_args(argc, argv) };
+    match read_dmm_file(args.get(0usize).map(ByondValue::clone).unwrap_or_default()) {
         Ok(val) => val,
-        Err(e) => {
-            let error_string = ::byondapi::value::ByondValue::try_from({
-                let res = std::fmt::format(format_args!("{:?}", e));
-                res
-            })
-            .unwrap();
-            ::byondapi::global_call::call_global_id(
-                {
-                    static STRING_ID: ::std::sync::OnceLock<u32> = ::std::sync::OnceLock::new();
-                    *STRING_ID.get_or_init(|| {
-                        ::byondapi::byond_string::str_id_of("_stack_trace").unwrap()
-                    })
-                },
-                &[error_string],
-            )
-            .unwrap();
-            ::byondapi::value::ByondValue::null()
+        Err(info) => {
+            crate::dm_call_stack_trace(format!("RUST BAPI ERROR \n {:#?}", info));
+            ByondValue::null()
         }
     }
 }
 
-fn read_dmm_file(path: ByondValue) -> ::eyre::Result<::byondapi::value::ByondValue> {
+fn read_dmm_file(path: ByondValue) -> eyre::Result<ByondValue> {
     setup_panic_handler();
     let path: String = path.get_string()?;
     // if !path.ends_with(".dmm") {
@@ -71,6 +53,6 @@ fn read_dmm_file(path: ByondValue) -> ::eyre::Result<::byondapi::value::ByondVal
     // }
     let path: std::path::PathBuf = path.try_into()?;
     let dmm = dmmtools::dmm::Map::from_file(&path)?;
-    let str = map_to_string(&dmm).ok_or(byondapi::Error::UnknownByondError)?;
+    let str = map_to_string(&dmm)?;
     Ok(ByondValue::new_str(str)?)
 }
