bc2ab0886c387b3382a08a8f01510b2a49e0c2f2
naut <nautilussplat@gmail.com>
2024-11-25 21:52:40 +0800

    sql migration

--- SQL/migrate-2023/V011__codeside_cargo.sql
+++ SQL/migrate-2023/V011__codeside_cargo.sql
@@ -1,11 +1,14 @@
 --
--- Fixes death saving again by removing the foreign key constraints on the ckeys. (sometimes chey can contain things that arnt ckeys. i.e. @ckey when someone aghosts)
+-- Creates a new cargo order table for use with new codeside cargo (#20030)
 --
-CREATE TABLE cargo_item_order_log (
-    id SERIAL PRIMARY KEY,
-    cargo_orderlog_id,
-    item_name,
-    amount,
+
+ALTER TABLE ss13_cargo_orderlog
+    ADD INDEX (order_id);
+
+CREATE TABLE  IF NOT EXISTS `cargo_item_order_log` (
+    id SERIAL PRIMARY KEY, -- Automatically creates an auto-incrementing unique identifier
+    cargo_orderlog_id INT(11), -- Replace INT with the correct data type for this column if needed
+    item_name VARCHAR(255) NOT NULL, -- Replace VARCHAR(255) with the correct data type/length
+    amount INT NOT NULL, -- Replace INT with the correct data type if needed
     FOREIGN KEY (cargo_orderlog_id) REFERENCES ss13_cargo_orderlog (order_id) ON DELETE CASCADE
 );
-
