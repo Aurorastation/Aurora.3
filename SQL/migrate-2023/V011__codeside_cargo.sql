58bda7d8cf6ea87dd0a6223d77a424351872f2f7
naut <nautilussplat@gmail.com>
2024-12-28 11:42:58 +0800

    sql frickery

--- SQL/migrate-2023/V011__codeside_cargo.sql
+++ SQL/migrate-2023/V011__codeside_cargo.sql
@@ -2,9 +2,12 @@
 -- Creates a new cargo order table for use with new codeside cargo (#20030)
 --
 
+ALTER TABLE ss13_cargo_orderlog
+    ADD INDEX (id);
+
 CREATE TABLE  IF NOT EXISTS `ss13_cargo_item_orderlog` (
     id SERIAL PRIMARY KEY,
-    cargo_orderlog_id INT(11),
+    cargo_orderlog_id INT(10) UNSIGNED NOT NULL,
     item_name VARCHAR(255) NOT NULL,
     amount INT NOT NULL,
     FOREIGN KEY (cargo_orderlog_id) REFERENCES ss13_cargo_orderlog (id) ON DELETE CASCADE
