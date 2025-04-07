77d1b6503773cdc188c56d98da6bdb366c72cc5a
DreamySkrell <>
2025-04-07 19:06:43 +0200

    a

--- rust/bapi/src/logging/mod.rs
+++ rust/bapi/src/logging/mod.rs
@@ -12,10 +12,30 @@ pub(crate) fn dm_call_stack_trace(msg: String) -> eyre::Result<()> {
 
 /// Panic handler, called on unhandled errors.
 /// Writes panic info to a text file, and calls dm stack trace proc as well.
-fn setup_panic_handler() {
+pub(crate) fn setup_panic_handler() {
     std::panic::set_hook(Box::new(|info| {
-        let msg = format!("RUST BAPI PANIC \n {:#?}", info);
-        let _ = std::fs::write("./bapi_panic.txt", msg.clone());
-        crate::dm_call_stack_trace(msg);
-    }))
+        let msg = format!("RUST BAPI PANIC \n {info}");
+        let msg_copy = msg.clone();
+        let _ = thread_sync(
+            || -> ByondValue {
+                if let Err(error) = dm_call_stack_trace(msg_copy) {
+                    let second_msg = format!("RUST BAPI PANIC; BYOND ERROR \n {:#?}", error);
+                    let _ = std::fs::write(
+                        Utc::now()
+                            .format("./bapi_dm_trace_failed_%Y%m%d_%H%M%S.txt")
+                            .to_string(),
+                        second_msg.clone(),
+                    );
+                }
+                Default::default()
+            },
+            true,
+        );
+        let _ = std::fs::write(
+            Utc::now()
+                .format("./bapi_panic_%Y%m%d_%H%M%S.txt")
+                .to_string(),
+            msg.clone(),
+        );
+    }));
 }
