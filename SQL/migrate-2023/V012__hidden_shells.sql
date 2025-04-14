d6028490892a2f4035ed38658c7f40f03f79cb27
FlamingLily <realFlamingLily@gmail.com>
2025-04-14 19:45:33 +1000

    oopsie missed some stuff

--- SQL/migrate-2023/V012__hidden_shells.sql
+++ SQL/migrate-2023/V012__hidden_shells.sql
@@ -1,5 +1,5 @@
 --
--- Adds character heights
+-- Adds hidden shell status
 --
 
 ALTER TABLE `ss13_characters_ipc_tags` ADD COLUMN `hidden_status` TINYINT(1) NOT NULL DEFAULT 0 AFTER `ownership_status`
