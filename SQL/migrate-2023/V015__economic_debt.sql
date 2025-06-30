7ba08adab89ae530587cf60dc4745e20d48af400
Greenjoe12345 <Greenjoe12345@gmail.com>
2025-06-30 16:26:40 +0100

    change fixes

--- SQL/migrate-2023/V015__economic_debt.sql
+++ SQL/migrate-2023/V015__economic_debt.sql
@@ -1,3 +1,3 @@
 -- Adds some new economic statuses - Bankrupt, and various stages of debt.
 --
-ALTER TABLE `ss13_characters` MODIFY COLUMN `economic_status` ENUM('Wealthy', 'Well-off', 'Average', 'Underpaid', 'Poor', 'Impoverished', 'Ruined', 'Bankrupt', 'Minor debt', 'Moderate debt', 'Major debt');
+ALTER TABLE `ss13_characters` MODIFY COLUMN `economic_status` ENUM('Wealthy', 'Well-off', 'Average', 'Underpaid', 'Poor', 'Impoverished', 'Ruined', 'Bankrupt', 'Minor Debt', 'Moderate Debt', 'Major Debt');
