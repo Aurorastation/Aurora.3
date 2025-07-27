306811a39d90c387e735e5573c46e4dc4d1a82aa
FabianK3 <21039694+FabianK3@users.noreply.github.com>
2025-07-27 17:04:45 +0200

    Update SQL date column names

--- SQL/migrate-2023/V017__persistence.sql
+++ SQL/migrate-2023/V017__persistence.sql
@@ -2,9 +2,9 @@ CREATE TABLE ss13_persistent_data (
     id INT NOT NULL AUTO_INCREMENT,
     author_ckey VARCHAR(32) NOT NULL,
     type VARCHAR(32) NOT NULL,
-    created_date DATETIME NOT NULL,
-    modified_date DATETIME NOT NULL,
-    expire_date DATETIME NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    expires_at DATETIME NULL,
     content JSON NOT NULL,
     PRIMARY KEY (id),
     INDEX idx_expireDate (expireDate)
