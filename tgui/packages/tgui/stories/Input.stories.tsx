f6a27db84c3aa67960822c8636738fd28b68eef7
Batrachophreno <batrochophreno@gmail.com>
2025-11-05 15:02:58 -0500

    Modals, stories

--- tgui/packages/tgui/stories/Input.stories.tsx
+++ tgui/packages/tgui/stories/Input.stories.tsx
@@ -59,65 +59,6 @@ function Story() {
             onChange={(value) => setNumber(value)}
           />
         </LabeledList.Item>
-        <LabeledList.Item label="Slider (onDrag)">
-          <Slider
-            step={1}
-            stepPixelSize={5}
-            value={number}
-            minValue={-100}
-            maxValue={100}
-            onChange={(value) => setNumber(value)}
-          />
-        </LabeledList.Item>
-        <LabeledList.Item label="Knob (onDrag)">
-          <Knob
-            inline
-            size={1}
-            step={1}
-            stepPixelSize={2}
-            value={number}
-            minValue={-100}
-            maxValue={100}
-            onChange={(value) => setNumber(value)}
-          />
-          <Knob
-            ml={1}
-            inline
-            bipolar
-            size={1}
-            step={1}
-            stepPixelSize={2}
-            value={number}
-            minValue={-100}
-            maxValue={100}
-            onChange={(value) => setNumber(value)}
-          />
-        </LabeledList.Item>
-        <LabeledList.Item label="Rotating Icon">
-          <Box inline position="relative">
-            <DraggableControl
-              value={number}
-              minValue={-100}
-              maxValue={100}
-              dragMatrix={[0, -1]}
-              step={1}
-              stepPixelSize={5}
-              onChange={(value) => setNumber(value)}
-            >
-              {(control) => (
-                <Box onMouseDown={control.handleDragStart}>
-                  <Icon
-                    size={4}
-                    color="yellow"
-                    name="times"
-                    rotation={control.displayValue * 4}
-                  />
-                  {control.inputElement}
-                </Box>
-              )}
-            </DraggableControl>
-          </Box>
-        </LabeledList.Item>
       </LabeledList>
     </Section>
   );
