442ec68e07b47c7b39bd9145a0abea719f9644b2
Batrachophreno <batrochophreno@gmail.com>
2025-11-09 14:05:37 -0500

    nuclear option

--- tools/build/juke/index.js
+++ tools/build/juke/index.js
@@ -5185,6 +5185,7 @@ var Worker = class {
       this.emitter.emit('fail');
       return;
     }
+    /*
     if (this.target.onlyWhen) {
       const result = await this.target.onlyWhen(this.context);
       if (!result) {
@@ -5194,6 +5195,7 @@ var Worker = class {
       }
       this.debugLog('Needs rebuild based on onlyWhen condition');
     }
+*/
     this.debugLog('Comparing inputs and outputs');
     const fileMapper = async (fileIo) => {
       const optionalPaths =
