ad09c3accc916cc89deceb4e0f77edb3715f214e
Werner <Arrow768@users.noreply.github.com>
2025-12-16 01:32:38 +0100

    Revert "Test Build Version Bump"
    This reverts commit 028a84e9374f65bef1917a45ade028d7108354a8.
    Revert "Build: Install libcurl3"
    
    This reverts commit c88a4f2e03b8ef117272ea829a9297dbe43af6af.
    Revert "Change libcurl3 to i386 version"
    
    This reverts commit 7c41a35a94f0b9ea0aea7a35c1dfae11cd80d090.
    Revert "Build: change curl package"
    
    This reverts commit 8b9a306fecdfcef0102e606e1907861cf056b19f.
    Revert "Build: add another dependency"
    
    This reverts commit 7dd81d1bd8a8071473252f3732e56ef0fe48ef98.
    Revert "Build: another package change"
    
    This reverts commit 28dea1605a8f3e4c4b329825ff5f0d7a8f6fae66.

--- tools/ci/install_byond.sh
+++ tools/ci/install_byond.sh
@@ -14,7 +14,7 @@ else
   rm -rf "$HOME/BYOND"
   mkdir -p "$HOME/BYOND"
   cd "$HOME/BYOND"
-  curl "https://buildassets.aurorastation.org/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip -H "Authorization: Basic $AURORA_BUILDASSETS_AUTH"
+  curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
   unzip byond.zip
   rm byond.zip
   cd byond
