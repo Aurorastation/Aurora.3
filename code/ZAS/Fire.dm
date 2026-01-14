Aurorastation/Aurora.3

commit cca0dfe7b6c39baa113cc1fbc194eed1de1466ba

Ports /tg/ and CM planemasters to replace our renderers. (#21458)
Part one of our nefarious plans to destroy lag forever, and also
accomplish some cool shit.

This replaces our renderer system with plane masters, this will also be
used to turn skyboxes into backdrops and thus totally remove skybox
updating lag. Additionally, this will let us manipulate entire planes
very easily to do all sorts of zany shit. All credit goes to the
original coders, this is some seriously cool stuff.

Also fixes some bizarre smoothing behaviour, reduces updateoverlays
calls since now not every single structure in the game tries smoothing
with nothing.

<img width="1349" height="1349" alt="image"
src="https://github.com/user-attachments/assets/9b7cecd7-3c47-448b-9dd8-9b904640bf82"
/>
<img width="1349" height="1349" alt="image"
src="https://github.com/user-attachments/assets/647d75a2-1bff-45ca-ab92-0aea10631afd"
/>

---------

Co-authored-by: Matt Atlas <liermattia@gmail.com>

code/ZAS/Fire.dm
@@ -132,12 +132,10 @@ If it gains pressure too slowly, it may leak or just rupture instead of explodin
 	icon = 'icons/effects/fire.dmi'
 	icon_state = "3"
 	appearance_flags = PIXEL_SCALE | NO_CLIENT_COLOR
-	render_target = HEAT_EFFECT_TARGET
 	mouse_opacity = MOUSE_OPACITY_TRANSPARENT
 
 /obj/fire
 	//Icon for fire on turfs.
-
 	anchored = 1
 	mouse_opacity = MOUSE_OPACITY_TRANSPARENT
 

code/__DEFINES/layers.dm
@@ -16,36 +16,35 @@
 	FLOAT_PLANE = -32767
 */
 
-#define LOWEST_PLANE -200
+/// NEVER HAVE ANYTHING BELOW THIS PLANE ADJUST IF YOU NEED MORE SPACE
+#define LOWEST_EVER_PLANE -200
 
-#define CLICKCATCHER_PLANE -100
+#define CLICKCATCHER_PLANE -92
 
-#define SPACE_PLANE -99
+#define SPACE_PLANE -91
 	#define SPACE_LAYER 1
 
-#define SKYBOX_PLANE -98
+#define SKYBOX_PLANE -90
 	#define SKYBOX_LAYER 1
 
-#define DUST_PLANE -97
+#define DUST_PLANE -82
 	#define DEBRIS_LAYER 1
 	#define DUST_LAYER 2
 
+#define OPENSPACE_BACKDROP_PLANE -81
+#define OPEN_SPACE_PLANE_START -80
 // Openspace uses planes -80 through -70.
-#define OPENTURF_MAX_PLANE -70
+// Do no put anything between these two, adjust more z level support as needed
+#define OPEN_SPACE_PLANE_END -70
 #define OPENTURF_MAX_DEPTH 10		// The maxiumum number of planes deep we'll go before we just dump everything on the same plane.
 
-#define OVER_OPENSPACE_PLANE -4
-
 #define HEAT_EFFECT_PLANE -4
-#define HEAT_EFFECT_TARGET    "*heat"
-#define COLD_EFFECT_TARGET    "*cold"
-#define COLD_EFFECT_BACK_TARGET "*coldb"
-#define HEAT_COMPOSITE_TARGET "*heatc"
 #define WARP_EFFECT_PLANE -3
 
 #define BLACKNESS_PLANE 0 //Blackness plane as per DM documentation.
 
-#define DEFAULT_PLANE 1
+/// Game Plane, where most of the game objects reside
+#define GAME_PLANE 1
 	#define PLATING_LAYER 1
 	//ABOVE PLATING
 	#define HOLOMAP_LAYER 1.01
@@ -135,10 +134,8 @@
 	#define POINTER_LAYER 4.27
 	#define MIMICED_LIGHTING_LAYER 4.28 // Z-Mimic-managed lighting
 
-	//FLY_LAYER 5
-	//OBSERVER
-	#define OBSERVER_LAYER 5.1
 	#define OBFUSCATION_LAYER 5.2
+	//FLY_LAYER 5
 
 	#define OVERMAP_SECTOR_LAYER 60
 	#define OVERMAP_IMPORTANT_SECTOR_LAYER 61
@@ -147,27 +144,37 @@
 
 	#define AREA_LAYER 999
 
-#define OBSERVER_PLANE 3
+/// Above Game Plane. For things which are above game objects, but below screen effects.
+#define ABOVE_GAME_PLANE 2
 
-#define ROOF_PLANE 4
+#define ROOF_PLANE 3
 
-#define LIGHTING_PLANE 5
+#define DISPLACEMENT_PLATE_RENDER_LAYER 4
+#define DISPLACEMENT_PLATE_RENDER_TARGET "*DISPLACEMENT_PLATE_RENDER_TARGET"
+
+#define GHOST_PLANE 80
+
+#define LIGHTING_PLANE 100
 	#define LIGHTING_LAYER 1
 
-#define EFFECTS_ABOVE_LIGHTING_PLANE 6
+#define BALLOON_CHAT_PLANE 110
+
+#define ABOVE_LIGHTING_PLANE 150
 	#define EYE_GLOW_LAYER 1
 	#define BEAM_PROJECTILE_LAYER 2
 	#define SUPERMATTER_WALL_LAYER 3
 	#define LIGHTNING_LAYER 4
 
-#define FULLSCREEN_PLANE 7
+#define RUNECHAT_PLANE 501
+
+#define FULLSCREEN_PLANE 9
 	#define FULLSCREEN_LAYER 1
 	#define DAMAGE_LAYER 2
 	#define IMPAIRED_LAYER 3
 	#define BLIND_LAYER 4
 	#define CRIT_LAYER 5
 
-#define HUD_PLANE 8
+#define HUD_PLANE 1000
 	#define UNDER_HUD_LAYER 1
 	#define HUD_BASE_LAYER 2
 	#define HUD_BELOW_ITEM_LAYER 2.9
@@ -177,32 +184,35 @@
 	#define RADIAL_BASE_LAYER 6
 	#define RADIAL_CONTENT_LAYER 7
 
-#define BALLOON_CHAT_PLANE 9
+#define CINEMATIC_PLANE 1200
+
+/*=============================*\
+| |
+|   PLANE DEFINES |
+| |
+\*=============================*/
+
+#define RENDER_PLANE_GAME 990
+#define RENDER_PLANE_NON_GAME 995
+
+#define RENDER_PLANE_MASTER 999
+
+/// Plane master controller keys
+#define PLANE_MASTERS_GAME "plane_masters_game"
+#define PLANE_MASTERS_NON_MASTER "plane_masters_non_master"
+
+//---------- EMISSIVES -------------
+//Layering order of these is not particularly meaningful.
+//Important part is the seperation of the planes for control via plane_master
 
 /// This plane masks out lighting, to create an "emissive" effect for e.g glowing screens in otherwise dark areas.
-#define EMISSIVE_PLANE 10
+#define EMISSIVE_PLANE 90
 #define EMISSIVE_TARGET "*emissive"
 	/// The layer you should use when you -really- don't want an emissive overlay to be blocked.
 	#define EMISSIVE_LAYER_UNBLOCKABLE 9999
 
-#define RUNECHAT_PLANE 500
-
-//-------------------- Rendering ---------------------
-
-/// Semantics - The final compositor or a filter effect renderer
-#define RENDER_GROUP_NONE null
-
-/// Things to be drawn within the game context
-#define RENDER_GROUP_SCENE 990
-
-/// Things to be drawn within the screen context
-#define RENDER_GROUP_SCREEN 995
-
-/// The final render group, for compositing
-#define RENDER_GROUP_FINAL 999
-
-/// Integer (One of `*_PLANE`). The atom's rendering plane. See `code\__defines\__renderer.dm` for a list of valid planes. Also see the DM Reference for `plane var (atom)`.
-/atom/plane = DEFAULT_PLANE
+/// The render target used by the emissive layer.
+#define EMISSIVE_RENDER_TARGET "*EMISSIVE_PLANE"
 
 #define DEFAULT_APPEARANCE_FLAGS (PIXEL_SCALE)
 
@@ -215,11 +225,11 @@
 	layer = HUD_ITEM_LAYER
 
 /image/proc/turf_decal_layerise()
-	plane = DEFAULT_PLANE
+	plane = GAME_PLANE
 	layer = DECAL_LAYER
 
 /image/proc/plating_decal_layerise()
-	plane = DEFAULT_PLANE
+	plane = GAME_PLANE
 	layer = DECAL_PLATING_LAYER
 
 /atom/proc/reset_plane_and_layer()

code/modules/effects/particles/native_particles.dm
@@ -82,63 +82,3 @@
 	layer = BELOW_TABLE_LAYER
 	alpha = 128
 
-
-//Spawner object
-//Maybe we could pool them in and out
-
-/obj/particle_emitter
-	name = ""
-	anchored = TRUE
-	mouse_opacity = 0
-	appearance_flags = PIXEL_SCALE
-	var/particle_type = null
-
-
-/obj/particle_emitter/Initialize(mapload, time, _color)
-	. = ..()
-	if (particle_type)
-		particles = GLOB.all_particles[particle_type]
-
-	if (time > 0)
-		QDEL_IN(src, time)
-	color = _color
-
-/obj/particle_emitter/heat
-	particle_type = "heat"
-	render_target = HEAT_EFFECT_TARGET
-	appearance_flags = PIXEL_SCALE | NO_CLIENT_COLOR
-
-
-/obj/particle_emitter/heat/Initialize()
-	. = ..()
-	filters += filter(type = "blur", size = 1)
-
-/obj/particle_emitter/heat/high
-	particle_type = "high heat"
-
-
-/obj/particle_emitter/mist
-	particle_type = "mist"
-	layer = FIRE_LAYER
-
-/obj/particle_emitter/mist/back
-	particle_type = "mist_back"
-	layer = BELOW_OBJ_LAYER
-
-
-/obj/particle_emitter/mist/back/gas
-	render_target = COLD_EFFECT_BACK_TARGET
-
-/obj/particle_emitter/mist/back/gas/Initialize(mapload, time, _color)
-	. = ..()
-	filters += filter(type="alpha", render_source = COLD_EFFECT_TARGET, flags = MASK_INVERSE)
-
-//for cold gas effect
-/obj/particle_emitter/mist/gas
-	render_target = COLD_EFFECT_TARGET
-	var/obj/particle_emitter/mist/back/b = /obj/particle_emitter/mist/back/gas
-
-/obj/particle_emitter/mist/gas/Initialize(mapload, time, _color)
-	. = ..()
-	b = new b(null)
-	vis_contents += b

code/modules/rendering/plane_master.dm
@@ -1,0 +1,182 @@
+/atom/movable/screen/plane_master
+	screen_loc = "CENTER"
+	icon_state = "blank"
+	appearance_flags = PLANE_MASTER|NO_CLIENT_COLOR
+	blend_mode = BLEND_OVERLAY
+	plane = LOWEST_EVER_PLANE
+	var/show_alpha = 255
+	var/hide_alpha = 0
+
+	//--rendering relay vars--
+	///integer: what plane we will relay this planes render to
+	var/render_relay_plane = RENDER_PLANE_GAME
+	///bool: Whether this plane should get a render target automatically generated
+	var/generate_render_target = TRUE
+	///integer: blend mode to apply to the render relay in case you dont want to use the plane_masters blend_mode
+	var/blend_mode_override
+	///reference: current relay this plane is utilizing to render
+	var/obj/render_plane_relay/relay
+
+/atom/movable/screen/plane_master/proc/Show(override)
+	alpha = override || show_alpha
+
+/atom/movable/screen/plane_master/proc/Hide(override)
+	alpha = override || hide_alpha
+
+//Why do plane masters need a backdrop sometimes? Read https://secure.byond.com/forum/?post=2141928
+//Trust me, you need one. Period. If you don't think you do, you're doing something extremely wrong.
+/atom/movable/screen/plane_master/proc/backdrop(mob/mymob)
+	SHOULD_CALL_PARENT(TRUE)
+	if(!isnull(render_relay_plane))
+		relay_render_to_plane(mymob, render_relay_plane)
+
+///Contains most things in the game world
+/atom/movable/screen/plane_master/game_world
+	name = "game world plane master"
+	plane = GAME_PLANE
+	blend_mode = BLEND_OVERLAY
+
+/atom/movable/screen/plane_master/rendering_plate/game_world/Initialize(mapload, datum/hud/hud_owner)
+	. = ..()
+	add_filter("displacer", 1, displacement_map_filter(render_source = DISPLACEMENT_PLATE_RENDER_TARGET, size = 10))
+
+/atom/movable/screen/plane_master/game_world_above
+	name = "above game world plane master"
+	plane = ABOVE_GAME_PLANE
+
+/atom/movable/screen/plane_master/ghost
+	name = "ghost plane master"
+	plane = GHOST_PLANE
+	render_relay_plane = RENDER_PLANE_NON_GAME
+
+/// Plane master handling display of building roofs. They're meant to become invisible when inside a building.
+/atom/movable/screen/plane_master/roof
+	name = "roof plane master"
+	plane = ROOF_PLANE
+	appearance_flags = PLANE_MASTER
+	blend_mode = BLEND_OVERLAY
+
+/atom/movable/screen/plane_master/space
+	name = "space plane master"
+	plane = SPACE_PLANE
+	blend_mode = BLEND_OVERLAY
+
+/// Plane master handling skyboxes.
+/atom/movable/screen/plane_master/skybox
+	name = "skybox plane master"
+	plane = SKYBOX_PLANE
+	blend_mode = BLEND_MULTIPLY
+	appearance_flags = PLANE_MASTER
+
+/**
+ * Plane master handling byond internal blackness
+ * vars are set as to replicate behavior when rendering to other planes
+ * do not touch this unless you know what you are doing
+ */
+/atom/movable/screen/plane_master/blackness
+	name = "darkness plane master"
+	plane = BLACKNESS_PLANE
+	mouse_opacity = MOUSE_OPACITY_TRANSPARENT
+	blend_mode = BLEND_MULTIPLY
+	appearance_flags = PLANE_MASTER | NO_CLIENT_COLOR | PIXEL_SCALE
+//byond internal end
+
+/*!
+ * This system works by exploiting BYONDs color matrix filter to use layers to handle emissive blockers.
+ *
+ * Emissive overlays are pasted with an atom color that converts them to be entirely some specific color.
+ * Emissive blockers are pasted with an atom color that converts them to be entirely some different color.
+ * Emissive overlays and emissive blockers are put onto the same plane.
+ * The layers for the emissive overlays and emissive blockers cause them to mask eachother similar to normal BYOND objects.
+ * A color matrix filter is applied to the emissive plane to mask out anything that isn't whatever the emissive color is.
+ * This is then used to alpha mask the lighting plane.
+ */
+
+///Contains all lighting objects
+/atom/movable/screen/plane_master/lighting
+	name = "lighting plane master"
+	plane = LIGHTING_PLANE
+	mouse_opacity = MOUSE_OPACITY_TRANSPARENT
+	blend_mode = BLEND_MULTIPLY
+
+/atom/movable/screen/plane_master/lighting/Initialize()
+	. = ..()
+	add_filter("emissives", 1, alpha_mask_filter(render_source = EMISSIVE_RENDER_TARGET, flags = MASK_INVERSE))
+
+/**
+ * Handles emissive overlays and emissive blockers.
+ */
+/atom/movable/screen/plane_master/emissive
+	name = "emissive plane master"
+	plane = EMISSIVE_PLANE
+	mouse_opacity = MOUSE_OPACITY_TRANSPARENT
+	render_target = EMISSIVE_RENDER_TARGET
+	render_relay_plane = null
+
+/atom/movable/screen/plane_master/emissive/Initialize()
+	. = ..()
+	add_filter("em_block_masking", 1, color_matrix_filter(GLOB.em_mask_matrix))
+
+/atom/movable/screen/plane_master/above_lighting
+	name = "above lighting plane master"
+	plane = ABOVE_LIGHTING_PLANE
+	render_relay_plane = RENDER_PLANE_GAME
+
+/atom/movable/screen/plane_master/runechat
+	name = "runechat plane master"
+	plane = RUNECHAT_PLANE
+	appearance_flags = PLANE_MASTER
+	blend_mode = BLEND_OVERLAY
+	render_relay_plane = RENDER_PLANE_NON_GAME
+
+/atom/movable/screen/plane_master/runechat/backdrop(mob/mymob)
+	. = ..()
+	remove_filter("AO")
+	add_filter("AO", 1, drop_shadow_filter(x = 0, y = -2, size = 4, color = "#04080FAA"))
+
+/atom/movable/screen/plane_master/fullscreen
+	name = "fullscreen alert plane"
+	plane = FULLSCREEN_PLANE
+	render_relay_plane = RENDER_PLANE_NON_GAME
+	mouse_opacity = MOUSE_OPACITY_TRANSPARENT
+
+/atom/movable/screen/plane_master/hud
+	name = "HUD plane"
+	plane = HUD_PLANE
+	render_relay_plane = RENDER_PLANE_NON_GAME
+
+/atom/movable/screen/plane_master/cinematic
+	name = "cinematic plane"
+	plane = CINEMATIC_PLANE
+	render_relay_plane = RENDER_PLANE_NON_GAME
+
+/atom/movable/screen/plane_master/displacement
+	name = "displacement plane"
+	mouse_opacity = MOUSE_OPACITY_TRANSPARENT
+	plane = DISPLACEMENT_PLATE_RENDER_LAYER
+	render_target = DISPLACEMENT_PLATE_RENDER_TARGET
+	render_relay_plane = null
+
+/atom/movable/screen/plane_master/open_space
+	name = "open space plane"
+	plane = OPEN_SPACE_PLANE_END //aurora snowflake: our openspace system works bottom up, not top down like CM's
+
+/atom/movable/screen/plane_master/open_space/Initialize(mapload, offset)
+	name = "open space plane [offset]"
+	plane -= offset
+	. = ..()
+	add_filter("multizblur", 1, gauss_blur_filter(0.5 + 0.25 * (offset + 1)))
+
+/atom/movable/screen/plane_master/openspace_backdrop
+	name = "open space plane master"
+	plane = OPENSPACE_BACKDROP_PLANE
+	appearance_flags = PLANE_MASTER
+	blend_mode = BLEND_MULTIPLY
+	alpha = 255
+
+/atom/movable/screen/plane_master/openspace_backdrop/Initialize()
+	. = ..()
+	filters = list()
+	filters += filter(type = "drop_shadow", color = "#04080FAA", size = -10)
+	filters += filter(type = "drop_shadow", color = "#04080FAA", size = -15)
+	filters += filter(type = "drop_shadow", color = "#04080FAA", size = -20)

code/modules/rendering/render_plate.dm
@@ -1,0 +1,79 @@
+/*!
+ * Custom rendering solution to allow for advanced effects
+ * We (ab)use plane masters and render source/target to cheaply render 2+ planes as 1
+ * if you want to read more read the _render_readme.md
+ */
+
+/**
+ * Render relay object assigned to a plane master to be able to relay it's render onto other planes that are not it's own
+ */
+/obj/render_plane_relay
+	screen_loc = "1,1"
+	layer = -1
+	plane = 0
+	appearance_flags = PASS_MOUSE | NO_CLIENT_COLOR | KEEP_TOGETHER
+
+/**
+ * ## Rendering plate
+ *
+ * Acts like a plane master, but for plane masters
+ * Renders other planes onto this plane, through the use of render objects
+ * Any effects applied onto this plane will act on the unified plane
+ * IE a bulge filter will apply as if the world was one object
+ * remember that once planes are unified on a render plate you cant change the layering of them!
+ */
+/atom/movable/screen/plane_master/rendering_plate
+	name = "default rendering plate"
+
+
+///this plate renders the final screen to show to the player
+/atom/movable/screen/plane_master/rendering_plate/master
+	name = "master rendering plate"
+	plane = RENDER_PLANE_MASTER
+	render_relay_plane = null
+	generate_render_target = FALSE
+
+///renders general in charachter game objects
+/atom/movable/screen/plane_master/rendering_plate/game_world
+	name = "game rendering plate"
+	plane = RENDER_PLANE_GAME
+	render_relay_plane = RENDER_PLANE_MASTER
+
+///render plate for OOC stuff like ghosts, hud-screen effects, etc
+/atom/movable/screen/plane_master/rendering_plate/non_game
+	name = "non-game rendering plate"
+	plane = RENDER_PLANE_NON_GAME
+	render_relay_plane = RENDER_PLANE_MASTER
+
+/**
+ * Plane master proc called in backdrop() that creates a relay object, sets it as needed and then adds it to the clients screen
+ * Sets:
+ * * layer from plane to avoid z-fighting
+ * * plane to relay the render to
+ * * render_source so that the plane will render on this object
+ * * mouse opacity to ensure proper mouse hit tracking
+ * * name for debugging purposes
+ * Other vars such as alpha will automatically be applied with the render source
+ * Arguments:
+ * * mymob: mob whose plane is being backdropped
+ * * relay_plane: plane we are relaying this plane master to
+ */
+/atom/movable/screen/plane_master/proc/relay_render_to_plane(mob/mymob, relay_plane)
+	if(relay in mymob.client.screen) //backdrop can be called multiple times
+		return
+	if(!render_target && generate_render_target)
+		render_target = "*[name]: AUTOGENERATED RENDER TGT"
+	relay = new()
+	relay.render_source = render_target
+	relay.plane = relay_plane
+	relay.layer = (plane + abs(LOWEST_EVER_PLANE))*0.5 //layer must be positive but can be a decimal
+
+	if(blend_mode_override)
+		relay.blend_mode = blend_mode_override
+	else
+		relay.blend_mode = blend_mode
+	relay.mouse_opacity = mouse_opacity
+	relay.name = render_target
+	mymob.client.add_to_screen(relay)
+	if(blend_mode != BLEND_MULTIPLY)
+		blend_mode = BLEND_DEFAULT

code/modules/xgm/xgm_gas_data.dm
@@ -79,7 +79,6 @@ var/global/datum/xgm_gas_data/gas_data
 	desc = "You shouldn't be clicking this."
 	plane = HEAT_EFFECT_PLANE
 	gas_id = GAS_HEAT
-	render_source = HEAT_EFFECT_TARGET
 
 /obj/gas_overlay/heat/Initialize(mapload, gas)
 	. = ..()
@@ -87,15 +86,13 @@ var/global/datum/xgm_gas_data/gas_data
 	icon_state = null
 
 /obj/effect/gas_cold_back
-	render_source = COLD_EFFECT_BACK_TARGET
-	plane = DEFAULT_PLANE
+	plane = GAME_PLANE
 	layer = BELOW_OBJ_LAYER
 
 /obj/gas_overlay/cold
 	name = "gas"
 	desc = "You shouldn't be clicking this."
 	gas_id = GAS_COLD
-	render_source = COLD_EFFECT_TARGET
 	var/obj/effect/gas_cold_back/b = null
 
 /obj/gas_overlay/cold/Initialize(mapload, gas)

commit e300cf9bbf0c20e3823594ce8bdfecd299cfd422

Repaired several effects. (#21711)
Fully fixes #21686, as well as other assorted issues caused by #21458.


![dreamseeker_ACcsHOw9m3](https://github.com/user-attachments/assets/cceb66b8-a067-4aba-aa41-5bfd4784a37a)

![dreamseeker_ihjvvcZmzH](https://github.com/user-attachments/assets/abaecb2e-d156-47af-8482-c7ad85a49cf4)

code/ZAS/Fire.dm
@@ -128,11 +128,13 @@ If it gains pressure too slowly, it may leak or just rupture instead of explodin
 
 	return 0
 
+// Future low-quality heat effect
 /obj/heat
 	icon = 'icons/effects/fire.dmi'
 	icon_state = "3"
 	appearance_flags = PIXEL_SCALE | NO_CLIENT_COLOR
 	mouse_opacity = MOUSE_OPACITY_TRANSPARENT
+	render_target = HEAT_EFFECT_PLATE_RENDER_TARGET
 
 /obj/fire
 	//Icon for fire on turfs.

code/__DEFINES/atmos.dm
@@ -50,7 +50,7 @@
 #define CARBON_LIFEFORM_FIRE_DAMAGE     4
 
 #define FOGGING_TEMPERATURE (T0C + 5)
-#define MAX_FOG_TEMPERATURE (T0C - 50)
+#define MAX_FOG_TEMPERATURE (T0C - 70)
 
 // Phoron fire properties.
 #define PHORON_MINIMUM_BURN_TEMPERATURE    (T0C +  126) //400 K - autoignite temperature in tanks and canisters - enclosed environments I guess

code/__DEFINES/layers.dm
@@ -38,8 +38,13 @@
 #define OPEN_SPACE_PLANE_END -70
 #define OPENTURF_MAX_DEPTH 10		// The maxiumum number of planes deep we'll go before we just dump everything on the same plane.
 
-#define HEAT_EFFECT_PLANE -8
-#define WARP_EFFECT_PLANE -7
+#define HEAT_EFFECT_PLANE 3
+#define HEAT_EFFECT_PLATE_RENDER_TARGET "*HEAT_EFFECT_PLATE_RENDER_TARGET"
+#define HEAT_EFFECT_COMPOSITE_RENDER_TARGET "*HEAT_EFFECT_COMPOSITE_RENDER_TARGET"
+#define COLD_EFFECT_PLATE_RENDER_TARGET "*COLD_EFFECT_PLATE_RENDER_TARGET"
+#define COLD_EFFECT_BACK_PLATE_RENDER_TARGET "*COLD_EFFECT_BACK_PLATE_RENDER_TARGET"
+#define WARP_EFFECT_PLANE 2
+#define WARP_EFFECT_PLATE_RENDER_TARGET "*WARP_EFFECT_PLATE_RENDER_TARGET"
 
 /// Game Plane, where most of the game objects reside
 #define GAME_PLANE -6

code/modules/effects/particles/native_particles.dm
@@ -45,6 +45,14 @@
 	friction = 0.2
 	grow = 0.0015
 
+/particles/mist/back
+	name = "mist_back"
+	spawning = 7
+	position = generator("box", list(-20, -1), list(20, 20), UNIFORM_RAND)
+	lifespan = 6 SECONDS
+	fadein = 1.5 SECONDS
+
+
 /particles/heat
 	name = "heat"
 	width = 500
@@ -82,3 +90,61 @@
 	layer = BELOW_TABLE_LAYER
 	alpha = 128
 
+/obj/particle_emitter
+	name = ""
+	anchored = TRUE
+	mouse_opacity = 0
+	appearance_flags = PIXEL_SCALE
+	var/particle_type = null
+
+/obj/particle_emitter/Initialize(mapload, time, _color)
+	. = ..()
+	if (particle_type)
+		particles = GLOB.all_particles[particle_type]
+
+	if (time > 0)
+		QDEL_IN(src, time)
+	color = _color
+
+// Future medium-quality heat effect
+/obj/particle_emitter/heat
+	particle_type = "heat"
+	render_target = HEAT_EFFECT_PLATE_RENDER_TARGET
+	appearance_flags = PIXEL_SCALE | NO_CLIENT_COLOR
+
+
+/obj/particle_emitter/heat/Initialize()
+	. = ..()
+	add_filter("heat_blur", 1, gauss_blur_filter(1))
+
+// High-quality heat effect
+/obj/particle_emitter/heat/high
+	particle_type = "high heat"
+
+
+// Medium/High-quality cold effect
+/obj/particle_emitter/mist
+	particle_type = "mist"
+	layer = FIRE_LAYER
+
+/obj/particle_emitter/mist/back
+	particle_type = "mist_back"
+	layer = BELOW_OBJ_LAYER
+
+
+/obj/particle_emitter/mist/back/gas
+	render_target = COLD_EFFECT_BACK_PLATE_RENDER_TARGET
+
+/obj/particle_emitter/mist/back/gas/Initialize(mapload, time, _color)
+	. = ..()
+	add_filter("cold_alpha", 1, alpha_mask_filter(render_source = COLD_EFFECT_PLATE_RENDER_TARGET, flags = MASK_INVERSE))
+
+//for cold gas effect
+/obj/particle_emitter/mist/gas
+	render_target = COLD_EFFECT_PLATE_RENDER_TARGET
+	var/obj/particle_emitter/mist/back/b = /obj/particle_emitter/mist/back/gas
+
+/obj/particle_emitter/mist/gas/Initialize(mapload, time, _color)
+	. = ..()
+	b = new b(null)
+	add_vis_contents(b)

code/modules/heavy_vehicle/equipment/utility.dm
@@ -279,14 +279,14 @@
 	///For when targetting a single object, will create a warp beam
 	var/datum/beam = null
 	var/max_dist = 6
-	var/obj/effect/effect/warp/small/warpeffect = null
+	var/obj/effect/warp/small/warpeffect = null
 
 /obj/effect/ebeam/warp
 	plane = WARP_EFFECT_PLANE
 	appearance_flags = DEFAULT_APPEARANCE_FLAGS | TILE_BOUND | NO_CLIENT_COLOR
 	z_flags = ZMM_IGNORE
 
-/obj/effect/effect/warp/small
+/obj/effect/warp/small
 	plane = WARP_EFFECT_PLANE
 	appearance_flags = PIXEL_SCALE | NO_CLIENT_COLOR
 	icon = 'icons/effects/96x96.dmi'

code/modules/rendering/plane_master.dm
@@ -175,10 +175,46 @@
 
 /atom/movable/screen/plane_master/displacement
 	name = "displacement plane"
+	blend_mode = BLEND_ADD
 	mouse_opacity = MOUSE_OPACITY_TRANSPARENT
 	plane = DISPLACEMENT_PLATE_RENDER_LAYER
 	render_target = DISPLACEMENT_PLATE_RENDER_TARGET
 	render_relay_plane = null
+
+/atom/movable/screen/plane_master/warp
+	name = "warp plane"
+	blend_mode = BLEND_ADD
+	mouse_opacity = MOUSE_OPACITY_TRANSPARENT
+	plane = WARP_EFFECT_PLANE
+	render_target = WARP_EFFECT_PLATE_RENDER_TARGET
+	render_relay_plane = null
+
+/atom/movable/screen/plane_master/heat
+	name = "heat haze plane"
+	blend_mode = BLEND_ADD
+	mouse_opacity = MOUSE_OPACITY_TRANSPARENT
+	plane = HEAT_EFFECT_PLANE
+	render_target = HEAT_EFFECT_COMPOSITE_RENDER_TARGET
+	render_relay_plane = null
+
+	var/gas_heat_object
+	//Not actually handled in this plane, we just use this to spawn the particle emitter so byond's rendering system will source it.
+	var/gas_cold_object
+
+/atom/movable/screen/plane_master/heat/Initialize()
+	. = ..()
+	setup()
+
+/atom/movable/screen/plane_master/heat/proc/setup()
+	//TODO: Add graphical options. If players don't like heat haze, replace particles with icons
+	if (gas_heat_object)
+		remove_vis_contents(gas_heat_object)
+	if (gas_cold_object)
+		remove_vis_contents(gas_cold_object)
+	gas_heat_object = new /obj/particle_emitter/heat/high
+	gas_cold_object = new /obj/particle_emitter/mist/gas
+	add_vis_contents(gas_heat_object)
+	add_vis_contents(gas_cold_object)
 
 /atom/movable/screen/plane_master/open_space
 	name = "open space plane"

code/modules/rendering/render_plate.dm
@@ -42,6 +42,8 @@
 /atom/movable/screen/plane_master/rendering_plate/game_world/Initialize(mapload, datum/hud/hud_owner)
 	. = ..()
 	add_filter("displacer", 1, displacement_map_filter(render_source = DISPLACEMENT_PLATE_RENDER_TARGET, size = 10))
+	add_filter("warp", 1, displacement_map_filter(render_source = WARP_EFFECT_PLATE_RENDER_TARGET, size = 5))
+	add_filter("heat_haze", 1, displacement_map_filter(render_source = HEAT_EFFECT_COMPOSITE_RENDER_TARGET, size = 2.5))
 
 ///render plate for OOC stuff like ghosts, hud-screen effects, etc
 /atom/movable/screen/plane_master/rendering_plate/non_game

code/modules/xgm/xgm_gas_data.dm
@@ -79,6 +79,7 @@ var/global/datum/xgm_gas_data/gas_data
 	desc = "You shouldn't be clicking this."
 	plane = HEAT_EFFECT_PLANE
 	gas_id = GAS_HEAT
+	render_source = HEAT_EFFECT_PLATE_RENDER_TARGET
 
 /obj/gas_overlay/heat/Initialize(mapload, gas)
 	. = ..()
@@ -88,16 +89,18 @@ var/global/datum/xgm_gas_data/gas_data
 /obj/effect/gas_cold_back
 	plane = GAME_PLANE
 	layer = BELOW_OBJ_LAYER
+	render_source = COLD_EFFECT_BACK_PLATE_RENDER_TARGET
 
 /obj/gas_overlay/cold
 	name = "gas"
 	desc = "You shouldn't be clicking this."
 	gas_id = GAS_COLD
 	var/obj/effect/gas_cold_back/b = null
+	render_source = COLD_EFFECT_PLATE_RENDER_TARGET
 
 /obj/gas_overlay/cold/Initialize(mapload, gas)
 	. = ..()
 	icon = null
 	icon_state = null
 	b = new()
-	vis_contents += b
+	add_vis_contents(b)