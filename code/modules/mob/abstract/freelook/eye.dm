Aurorastation/Aurora.3

commit 58570e035a7603c30af145497b29247c2159a606

Adds base planning blueprints (#21626)
## About PR

Adds some building/planning mechanics typically seen in
base-building/survival games such as Grounded. Hopefully this will ease
the away base construction process. Happy base buildings!

Currently, this allows placing blueprint objects that players can click
on with the required materials to build whatever the blueprint
represents. You can place templates, edit them or build your own
glorious base from ground.

Sprites for `blueprints` and `blueprints_unfolded` inside
`base_planning_blueprints.dmi` are ported from SS20 Russian Paradise
Station.

## Images
https://youtu.be/FCrZn8BkISU?si=Fst0ksAgi47D_wN7

### Asset Licenses
The following assets that **have not** been created by myself are
included in this PR:

| Path | Original Author | License |
| --- | --- | --- |
| icons/obj/item/base_planning_blueprints.dmi | vovakons (SS20 Russian
Paradise Station) | AGPLv3 |

code/modules/mob/abstract/freelook/eye.dm
@@ -40,6 +40,9 @@
 		return EyeMove(n, direct)
 	return 0
 
+/mob/abstract/eye/can_ztravel(var/direction)
+	return TRUE
+
 /mob/abstract/eye/airflow_hit(atom/A)
 	airflow_speed = 0
 	airflow_dest = null
@@ -137,7 +140,7 @@
 
 	for(var/i = 0; i < max(sprint, initial); i += 20)
 		var/turf/step = get_turf(get_step(src, direct))
-		if(step)
+		if(step && check_allowed_movement(step))
 			setLoc(step)
 
 	cooldown = world.timeofday + 5
@@ -146,6 +149,10 @@
 	else
 		sprint = initial
 	return 1
+
+// If the eye movement needs to be restricted by any means, override this
+/mob/abstract/eye/proc/check_allowed_movement(turf/step)
+	return TRUE
 
 /mob/abstract/eye/ClickOn(atom/A, params)
 	if(owner)

code/modules/multiz/movement.dm
@@ -31,10 +31,6 @@
  *			FALSE otherwise.
  */
 /mob/proc/zMove(direction)
-	// In the case of an active eyeobj, move that instead.
-	if (eyeobj)
-		return eyeobj.zMove(direction)
-
 	// Check if we can actually travel a Z-level.
 	if (!can_ztravel(direction))
 		to_chat(src, SPAN_WARNING("You lack means of travel in that direction."))

aurorastation.dme
@@ -457,6 +457,7 @@
 #include "code\datums\position_point_vector.dm"
 #include "code\datums\progressbar.dm"
 #include "code\datums\records.dm"
+#include "code\datums\room_template.dm"
 #include "code\datums\ruins.dm"
 #include "code\datums\signals.dm"
 #include "code\datums\sound_player.dm"
@@ -478,6 +479,7 @@
 #include "code\datums\components\armor\armor.dm"
 #include "code\datums\components\base_name\base_name.dm"
 #include "code\datums\components\eye\_eye.dm"
+#include "code\datums\components\eye\base_planner.dm"
 #include "code\datums\components\eye\blueprints.dm"
 #include "code\datums\components\eye\freelook.dm"
 #include "code\datums\components\multitool\_multitool.dm"
@@ -1159,6 +1161,7 @@
 #include "code\game\objects\effects\temporary_visuals\temporary_visual.dm"
 #include "code\game\objects\items\airbubble.dm"
 #include "code\game\objects\items\apc_frame.dm"
+#include "code\game\objects\items\base_planning_blueprints.dm"
 #include "code\game\objects\items\blueprints.dm"
 #include "code\game\objects\items\bodybag.dm"
 #include "code\game\objects\items\camping.dm"
@@ -2625,6 +2628,7 @@
 #include "code\modules\mob\abstract\freelook\ai\chunk.dm"
 #include "code\modules\mob\abstract\freelook\ai\eye.dm"
 #include "code\modules\mob\abstract\freelook\ai\update_triggers.dm"
+#include "code\modules\mob\abstract\freelook\blueprints\base_planner.dm"
 #include "code\modules\mob\abstract\freelook\blueprints\blueprints.dm"
 #include "code\modules\mob\abstract\ghost\ghost.dm"
 #include "code\modules\mob\abstract\ghost\observer\logout.dm"

code/_onclick/hud/fullscreen.dm
@@ -159,6 +159,9 @@
 	screen_loc = ui_entire_screen
 	alpha = 100
 
+/atom/movable/screen/fullscreen/blueprints/less_alpha
+	alpha = 35
+
 /atom/movable/screen/fullscreen/lighting_backdrop
 	icon = 'icons/mob/screen/white.dmi'
 	icon_state = "flash"

code/datums/components/eye/base_planner.dm
@@ -1,0 +1,39 @@
+/datum/component/eye/base_planner
+	eye_type = /mob/abstract/eye/base_planner
+	action_type = /datum/action/eye/base_planner
+
+/datum/action/eye/base_planner
+	eye_type = /mob/abstract/eye/base_planner
+
+/datum/action/eye/base_planner/choose_blueprint_object
+	name = "Choose Blueprint Object"
+	procname = "choose_blueprint_object"
+	button_icon_state = "pencil"
+	target_type = EYE_TARGET
+
+/datum/action/eye/base_planner/place_template
+	name = "Place Template Schematic"
+	procname = "place_template"
+	button_icon_state = "template_insert"
+	target_type = EYE_TARGET
+
+/datum/action/eye/base_planner/erase_all
+	name = "Erase All Schematics"
+	procname = "erase_all"
+	button_icon_state = "eraser"
+	target_type = EYE_TARGET
+
+/datum/action/eye/base_planner/help
+	name = "Help"
+	procname = "help"
+	button_icon_state = "help"
+	target_type = COMPONENT_TARGET
+
+/datum/component/eye/base_planner/proc/help()
+	if(!current_looker)
+		return
+	to_chat(current_looker, SPAN_NOTICE("-***********************************************************-"))
+	to_chat(current_looker, SPAN_NOTICE("Left Click			= Place selected object blueprint."))
+	to_chat(current_looker, SPAN_NOTICE("Control Click		= Clear blueprint objects in selected turf."))
+	to_chat(current_looker, SPAN_NOTICE("Middle Mouse Click	= Copy the selected blueprint object as ."))
+	to_chat(current_looker, SPAN_NOTICE("*-*********************************************************-*"))

code/datums/room_template.dm
@@ -1,0 +1,218 @@
+/**
+ * A basic template loader system, allows placing objects, items, turfs in a specified area.
+ * Currently this function doesn't support the following:
+ *
+ * * Uploading templates in-game.
+ * * Even number area dimensions.
+ */
+/datum/room_template
+	var/name = "base template"
+
+	/// Top-down view of the room template, which can be edited like the example below:
+	/**
+	 * Define some vars that contains object-turf paths in New() like this:
+	 * var/W = /turf/simulated/wall
+	 * var/F = /turf/simulated/floor/plating
+	 * var/G = /obj/effect/map_effect/window_spawner/full/reinforced
+	 * var/A = /obj/machinery/door/airlock
+	 * var/list/AF = list(A, F)
+	 *
+	 * then:
+	 * layouts = list(
+	 *	list(
+	 *		list(W, G, W, W, W),
+	 *		list(W, F, F, F, W),
+	 *		list(W, W, W, AF, W)
+	 *	) = "average college student dorm",
+	 * )
+	 * This is a layout of a 5x3 (5 width, 3 height) room. Vars can contain whatever path you want and you can fit multiple layouts in here.
+	 * You can also enter values to the keys in this list, it'll be displayed in template selection UI. Make sure each value is unique.
+	 * This system also supports placing multiple atoms per turf, needs to be defined inside a list on the turf represented in layout, see the example above.
+	 * Keep the area dimension as odd numbers, reason explained below.
+	 */
+	var/list/layouts
+
+	/// Lists for keeping record of template related placements, incase the user wants to revert it.
+	var/list/placed_template_objects = list()
+	var/list/replaced_turfs = list()
+	var/list/deleted_blueprints = list()
+
+/datum/room_template/proc/template_selection(turf/center)
+	var/list/options = list()
+	var/list/chosen_layout = list()
+
+	for(var/key in layouts)
+		options += layouts[key]
+
+	var/choice = tgui_input_list(usr, "Choose a template to place.", "Template Selection", options)
+
+	if(!choice)
+		return
+
+	for(var/key in layouts)
+		if(layouts[key] == choice) // this is the reason every value needs to be unique
+			chosen_layout = key
+			break
+
+	if(chosen_layout)
+		build_the_template(center, chosen_layout)
+
+/datum/room_template/proc/build_the_template(turf/center, list/chosen_layout)
+	var/height = length(chosen_layout)
+	var/width = length(chosen_layout[1])
+	var/starting_x = center.x - round(width / 2) // this system assumes it's always at the center, hence even numbers aren't supported
+	var/starting_y = center.y + round(height / 2)
+	var/starting_z = center.z
+
+	if(width % 2 == 0 || height % 2 == 0)
+		CRASH("Room template [name] has a layout with even dimensions as width or height. Only odd numbers are supported.")
+
+	for(var/row in 1 to height)
+		for(var/column in 1 to width)
+			// The math and reasoning behind this is, due to how we arrange reading the layout list
+			// (consider the top-down view, and now visualise the list getting flattened), we start placing the template from top-left corner to bottom right.
+			// We simply pick a turf that is specified by width*height area, and see where this exact turf corrresponds to in the layout given to us.
+			var/turf/T = locate(starting_x + column - 1, starting_y - row + 1, starting_z) // we're locating while assuming we're in the center
+			if(!T || T.density)
+				continue
+
+			var/tile_data = chosen_layout[row][column]
+			if(!tile_data)
+				continue
+
+			for(var/obj/structure/blueprint/colliding_blueprint in T)
+				deleted_blueprints += list(list("turf" = T, "type" = colliding_blueprint.type))
+				qdel(colliding_blueprint)
+
+			if(islist(tile_data))
+				for(var/i in tile_data)
+					place_tile_data(T, i)
+			else
+				place_tile_data(T, tile_data)
+
+	// Finalizing template placement
+	if(usr?.client)
+		if(tgui_alert(usr, "Would you like to keep the placed template?", "Template Confirmation", list("Confirm", "Cancel")) != "Confirm")
+			QDEL_LAZYLIST(placed_template_objects) // revert placed objects
+
+			for(var/turf/T in replaced_turfs) // restore turfs
+				T.ChangeTurf(replaced_turfs[T])
+
+			for(var/tile in deleted_blueprints) // restore blueprints
+				var/blueprint_to_restore = tile["type"]
+				new blueprint_to_restore(tile["turf"])
+
+			to_chat(usr, SPAN_NOTICE("Template has been reverted."))
+
+	replaced_turfs = null
+	deleted_blueprints = null
+
+/datum/room_template/proc/place_tile_data(turf/T, tile_data)
+	if(ispath(tile_data, /turf)) // Turf
+		replaced_turfs[T] = T.type
+		T.ChangeTurf(tile_data)
+		return
+
+	placed_template_objects += new tile_data(T) // Object or anything else
+
+// ---- Subtypes
+
+// Base Planner Templates
+/datum/room_template/base_planner_template
+	name = "Base Planner Template"
+
+/datum/room_template/base_planner_template/New()
+	var/G = /obj/structure/blueprint/girder
+	var/W = /obj/structure/blueprint/window_frame
+	var/A = /obj/structure/blueprint/airlock_frame
+	var/F = /obj/structure/blueprint/turf_plating
+	var/list/X = list(A, F)
+	var/N
+
+	layouts = list(
+		list(
+			list(G, G ,G, G, G),
+			list(G, F, F, F, G),
+			list(G, F, F, F, G),
+			list(G, F, F, F, G),
+			list(G, G, G, G, G),
+		) = "5x5 Room",
+
+		list(
+			list(G, G ,G, G, G, G, G),
+			list(G, F, F, F, F, F, G),
+			list(G, F, F, F, F, F, G),
+			list(G, F, F, F, F, F, G),
+			list(G, F, F, F, F, F, G),
+			list(G, F, F, F, F, F, G),
+			list(G, G, G, G, G, G, G),
+		) = "7x7 Room",
+
+		list(
+			list(N, G, G, X, W, X, G, G, N),
+			list(G, G, F, F, F, F, F, G, G),
+			list(G, F, F, F, F, F, F, F, G),
+			list(W, F, F, F, F, F, F, F, W),
+			list(W, F, F, F, F, F, F, F, W),
+			list(W, F, F, F, F, F, F, F, W),
+			list(G, F, F, F, F, F, F, F, G),
+			list(G, G, F, F, F, F, F, G, G),
+			list(N, G, G, X, W, X, G, G, N)
+		) = "9x9 Room with Windows and Airlocks",
+
+		list(
+			list(N, G ,G, G, X, W, X, G, G, G, N),
+			list(G, G ,F, F, F, F, F, F, F, G, G),
+			list(G, F ,F, F, F, F, F, F, F, F, G),
+			list(W, F ,F, F, F, F, F, F, F, F, W),
+			list(W, F ,F, F, F, F, F, F, F, F, W),
+			list(X, F ,F, F, F, F, F, F, F, F, X),
+			list(W, F ,F, F, F, F, F, F, F, F, W),
+			list(W, F ,F, F, F, F, F, F, F, F, W),
+			list(G, F ,F, F, F, F, F, F, F, F, G),
+			list(G, G ,F, F, F, F, F, F, F, G, G),
+			list(N, G ,G, G, X, W, X, G, G, G, N),
+		) = "11x11 Room with Windows and Airlocks",
+
+		list(
+			list(N, G ,W, A, A, G, G, G, G, G, N),
+			list(G, G ,F, F, F, G, F, F, F, G, G),
+			list(G, F ,F, F, F, G, F, F, F, F, W),
+			list(G, F ,F, F, F, X, F, F, F, F, X),
+			list(W, F ,F, F, F, W, F, F, F, F, G),
+			list(W, F ,F, F, F, G, G, F, F, F, G),
+			list(W, F ,F, F, F, F, G, G, W, G, G),
+			list(G, F ,F, F, F, F, W, F, F, F, G),
+			list(G, F ,F, F, F, F, X, F, F, F, G),
+			list(G, G ,F, F, F, F, G, F, F, G, G),
+			list(N, G ,W, X, X, G, G, G, G, G, N),
+		) = "11x11 Base Template - A",
+
+		list(
+			list(N, G ,G, G, X, W, X, G, G, G, N),
+			list(G, G ,F, G, F, F, F, G, F, G, G),
+			list(W, F ,F, W, F, F, F, W, F, F, W),
+			list(G, F ,F, X, F, F, F, X, F, F, G),
+			list(G, F ,F, G, F, F, F, G, F, F, G),
+			list(G, G ,G, G, F, F, F, G, G, G, G),
+			list(G, F ,F, G, F, F, F, G, F, F, G),
+			list(G, F ,F, X, F, F, F, X, F, F, G),
+			list(W, F ,F, W, F, F, F, W, F, F, W),
+			list(G, G ,F, G, F, F, F, G, F, G, G),
+			list(N, G ,G, G, X, W, X, G, G, G, N),
+		) = "11x11 Base Template - B",
+
+		list(
+			list(N, G ,X, X, W, G, G, W, W, G, N),
+			list(G, G ,F, F, F, F, F, F, F, G, G),
+			list(G, F ,F, F, F, F, F, F, F, F, G),
+			list(W, F ,F, F, F, F, F, F, F, F, G),
+			list(G, F ,F, F, F, F, G, W, X, G, G),
+			list(G, G ,X, X, G, G, G, F, F, F, G),
+			list(G, F ,F, F, F, G, F, F, F, F, G),
+			list(W, F ,F, F, F, X, F, F, F, F, X),
+			list(G, F ,F, F, F, X, F, F, F, F, X),
+			list(G, G ,F, F, F, G, F, F, F, G, G),
+			list(N, G ,G, G, G, G, G, X, X, G, N),
+		) = "11x11 Base Template - C"
+	)

code/game/objects/items/base_planning_blueprints.dm
@@ -1,0 +1,57 @@
+/obj/item/base_planning_blueprints
+	name = "base planning schematics"
+	desc = "A blueprint, detailing the structure plans for off-site operations."
+	icon = 'icons/obj/item/base_planning_blueprints.dmi'
+	icon_state = "blueprints"
+	attack_verb = list("attacked", "bapped", "hit")
+	w_class = WEIGHT_CLASS_SMALL
+	drop_sound = 'sound/items/drop/wrapper.ogg'
+	pickup_sound = 'sound/items/pickup/wrapper.ogg'
+
+	/// Whether the object is folded or not.
+	var/folded = TRUE
+	/// Z-levels that this object not allowed to function at, mainship Z-levels by default.
+	var/list/invalid_z_levels = list()
+
+/obj/item/base_planning_blueprints/mechanics_hints()
+	. += ..()
+	. += "You can Alt-Click this item on a table to unfold it."
+	. += "These schematics can be used to plan away base structures, comes with pre-made room templates."
+
+/obj/item/base_planning_blueprints/Initialize()
+	. = ..()
+	return INITIALIZE_HINT_LATELOAD
+
+/obj/item/base_planning_blueprints/LateInitialize()
+	invalid_z_levels = SSmapping.levels_by_trait(ZTRAIT_STATION)
+	AddComponent(/datum/component/eye/base_planner)
+
+/obj/item/base_planning_blueprints/AltClick(mob/user)
+	if(folded)
+		if(loc == user || !locate(/obj/structure/table) in get_turf(src))
+			to_chat(user, SPAN_WARNING("\The [src] needs to be placed on top of a table in order to be unfolded!"))
+			return
+
+		folded = FALSE
+		anchored = TRUE
+		icon_state = "blueprints_unfolded"
+	else
+		folded = TRUE
+		anchored = FALSE
+		icon_state = "blueprints"
+
+	playsound(loc, 'sound/bureaucracy/paperfold.ogg', 50, TRUE)
+
+/obj/item/base_planning_blueprints/attack_hand(mob/user)
+	if(folded)
+		return ..()
+
+	if(user.z in invalid_z_levels)
+		to_chat(user, SPAN_WARNING("Upon inspection, these schematics contain only groundside architectural designs, which aren't useful for your whereabouts!"))
+		return
+
+	var/datum/component/eye/blueprints = GetComponent(/datum/component/eye)
+	if(blueprints)
+		if(blueprints.look(user))
+			to_chat(user, SPAN_NOTICE("You lean forward, inspecting \the [src] attentively."))
+			return

code/game/objects/structures/crates_lockers/closets/secure/engineering.dm
@@ -31,6 +31,7 @@
 	new /obj/item/grenade/chem_grenade/antifuel(src)
 	new /obj/item/storage/lockbox/shuttle_blueprints(src)
 	new /obj/item/blueprints/outpost(src)
+	new /obj/item/base_planning_blueprints(src)
 
 // Chief Engineer - Clothing Satchel
 // This satchel is used nowhere except in conjunction with the locker above,

commit ba07ebeb3057d40e500ce0a9f6ad310766bfc7f4

Fixes AI eye being unable to traverse Z-levels (#21708)
## About PR

Fixes a bug introduced in #21626. I misunderstood the purpose of a check
which was allowing eyeobjects to Z-travel, my bad!

- Fixes #21695

code/modules/mob/abstract/freelook/eye.dm
@@ -150,7 +150,7 @@
 		sprint = initial
 	return 1
 
-// If the eye movement needs to be restricted by any means, override this
+/// If the eye movement needs to be restricted by any additional means, override this.
 /mob/abstract/eye/proc/check_allowed_movement(turf/step)
 	return TRUE
 

code/modules/multiz/movement.dm
@@ -31,6 +31,11 @@
  *			FALSE otherwise.
  */
 /mob/proc/zMove(direction)
+	// If the calling mob has an active eyeobj reference, we move it instead.
+	// This is important because zMove is called from the actual mob and not the eyeobj they're controlling.
+	if(eyeobj)
+		return eyeobj.zMove(direction)
+
 	// Check if we can actually travel a Z-level.
 	if (!can_ztravel(direction))
 		to_chat(src, SPAN_WARNING("You lack means of travel in that direction."))