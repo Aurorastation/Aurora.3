Aurorastation/Aurora.3

commit 0c342878e58aed237ef0e40853754f87d992e54f

Engineering spriting (detailing & port), portable shield balance, bugfixes (#21862)
EXTREMELY MINOR MISCELLANEOUS CHANGES TEEHEE

changes:
- imageadd: "Adds +bolted, +welded, +lights overlays to several large
machines for easier visual identification of whether or not clicking on
that emitter will kill you or not, etc."
- imageadd: "Modifies the base of the Shield Wall Generator sprite to
better match that of the Emergency Shield Projector (and so they can use
the same overlays mentioned above)."
- imageadd: "Tweaks the Emitter's directional sprites slightly so that
the base doesn't seem to change shape quite so drastically when
rotated."
- imageadd: "Ports very pretty space heater sprites made by Wallemations
and vinylspiders from /tg/."
  - imageadd: "Adds unique sprites for Old and Heavy-Duty power cells."
- balance: "Increases power draw and range of Emergency Shield
Projectors."
  - balance: "Increases power draw of Shield Wall Generators."
- balance: "Greatly reduces time for debugger to fix APCs corrupted by
electrical storms."
- balance: "Increases multi-z shield capacitor reserve charge and
maximum throughput, so that high-strength shields can actually be run
(at high power draw)."
- balance: "Tweaks the INDRA power output formula to scale better with
increasing temperature."
- balance: "Slight adjustments to space heater efficiency formula with a
friendly handwave to entropy."
- balance: "Old power cells have a 5% chance to be defective and have
slightly lower maxcharge than standard cells."
  - qol: "Adds extended examine hints to engineering objects."
- qol: "Extends list of object types which INDRA core field will
interact peacefully with."
- qol: "Improved space heater UX (chat msgs changed to balloon alerts,
audible feedback, etc.)"
  - code_imp: "Updates many code comments to use DMdocs."
- code_imp: "Consolidates various shield generation machine sprites in
scattered across multiple files into a single shield generator machines
sprite file."

code/modules/shieldgen/emergency_shield.dm
@@ -1,3 +1,255 @@
+/obj/machinery/shieldgen
+	name = "emergency shield projector"
+	desc = "Used to seal minor hull breaches."
+	icon = 'icons/obj/machinery/shielding.dmi'
+	icon_state = "shieldoff"
+	density = TRUE
+	opacity = TRUE
+	anchored = FALSE
+	req_access = list(ACCESS_ENGINE)
+	var/health = 100
+	var/active = FALSE
+	/// Malfunction causes parts of the shield to slowly dissipate
+	var/malfunction = FALSE
+	var/list/deployed_shields = list()
+	var/list/regenerating = list()
+	/// Range at which it can project shields.
+	var/range = 4
+	panel_open = FALSE
+	var/locked = FALSE
+	/// Periodically recheck if we need to rebuild a shield.
+	var/check_delay = 60
+	use_power = POWER_USE_OFF
+	idle_power_usage = 250
+
+/obj/machinery/shieldgen/mechanics_hints(mob/user, distance, is_adjacent)
+	. += ..()
+	. += "This machine's primary function is to project energy shields calibrated for atmospheric containment; these shields keep the air in and the vacuum out."
+	. += "If activated while within <b>[range]</b> tiles of a 'space' turf, an 'open space' turf, or an otherwise 'airless' turf, it will automatically project shields there, making it extremely useful for containing hull breaches."
+	. += "It will also project energy shields onto any engineering tape within range that has been toggled as a 'shield marker' using a multitool."
+	. += "ALT-click the [src] to lock or unlock it (if you have the appropriate ID access)."
+
+/obj/machinery/shieldgen/Destroy()
+	collapse_shields()
+	return ..()
+
+/obj/machinery/shieldgen/proc/shields_up()
+	if(active) return FALSE //If it's already turned on, how did this get called?
+
+	src.active = TRUE
+	update_icon()
+
+	create_shields()
+
+	var/shield_power_usage
+	for(var/obj/machinery/shield/shield_tile in deployed_shields)
+		shield_power_usage += shield_tile.shield_idle_power
+
+	change_power_consumption(shield_power_usage, POWER_USE_IDLE)
+	update_use_power(POWER_USE_IDLE)
+
+/obj/machinery/shieldgen/proc/shields_down()
+	if(!active) return FALSE //If it's already off, how did this get called?
+
+	src.active = FALSE
+	update_icon()
+
+	collapse_shields()
+
+	update_use_power(POWER_USE_OFF)
+
+/obj/machinery/shieldgen/proc/create_shields()
+	for(var/T in RANGE_TURFS(range, src))
+		var/turf/target_tile = T
+		if(locate(/obj/machinery/shield) in target_tile)
+			continue
+		var/obj/item/tape/engineering/E = locate() in target_tile
+		if(E?.shield_marker)
+			deploy_shield(target_tile)
+		else if(istype(target_tile,/turf/space) || istype(target_tile,/turf/simulated/open) || istype(target_tile,/turf/simulated/floor/exoplanet/asteroid/ash) || istype(target_tile,/turf/simulated/floor/airless))
+			if(malfunction && prob(33) || !malfunction)
+				deploy_shield(target_tile)
+
+/obj/machinery/shieldgen/proc/deploy_shield(var/turf/T)
+	var/obj/machinery/shield/shield_tile = new /obj/machinery/shield(T)
+	deployed_shields += shield_tile
+	use_power_oneoff(shield_tile.shield_generate_power)
+
+/obj/machinery/shieldgen/proc/collapse_shields()
+	for(var/obj/machinery/shield/shield_tile in deployed_shields)
+		deployed_shields -= shield_tile
+		if(!QDELETED(shield_tile))
+			qdel(shield_tile)
+	change_power_consumption(250, POWER_USE_IDLE)
+	update_use_power(POWER_USE_IDLE)
+
+/obj/machinery/shieldgen/power_change()
+	..()
+	if(!active) return
+	if(stat & NOPOWER)
+		collapse_shields()
+	else
+		create_shields()
+	update_icon()
+
+/obj/machinery/shieldgen/process()
+	if(stat & NOPOWER)
+		if(active)
+			visible_message(SPAN_WARNING("\The [src] shuts down due to a lack of power."), SPAN_NOTICE("You hear a heavy droning fade out."))
+			active = FALSE
+			collapse_shields()
+		return
+
+	if(malfunction)
+		if(deployed_shields.len && prob(5))
+			qdel(pick(deployed_shields))
+	else
+		if (check_delay <= 0)
+			create_shields()
+
+			var/new_power_usage = 0
+			for(var/obj/machinery/shield/shield_tile in deployed_shields)
+				new_power_usage += shield_tile.shield_idle_power
+
+			if (new_power_usage != idle_power_usage)
+				change_power_consumption(new_power_usage, POWER_USE_IDLE)
+				use_power_oneoff(0)
+
+			check_delay = 30
+		else
+			check_delay--
+
+/obj/machinery/shieldgen/proc/checkhp()
+	if(health <= 30)
+		src.malfunction = TRUE
+	if(health <= 0)
+		spawn(0)
+			explosion(get_turf(src.loc), 0, 0, 1, 0, 0, 0)
+		qdel(src)
+	update_icon()
+	return
+
+/obj/machinery/shieldgen/ex_act(severity)
+	switch(severity)
+		if(1.0)
+			src.health -= 75
+			src.checkhp()
+		if(2.0)
+			src.health -= 30
+			if (prob(15))
+				src.malfunction = 1
+			src.checkhp()
+		if(3.0)
+			src.health -= 10
+			src.checkhp()
+	return
+
+/obj/machinery/shieldgen/emp_act(severity)
+	. = ..()
+
+	switch(severity)
+		if(EMP_HEAVY)
+			src.health /= 2 //cut health in half
+			malfunction = TRUE
+			locked = pick(0,1)
+		if(EMP_LIGHT)
+			if(prob(50))
+				src.health *= 0.3 //chop off a third of the health
+				malfunction = TRUE
+	checkhp()
+
+/obj/machinery/shieldgen/attack_hand(mob/user)
+	if(locked)
+		to_chat(user, SPAN_WARNING("The machine is locked!"))
+		return
+	if(panel_open)
+		to_chat(user, SPAN_WARNING("The panel must be closed before operating this machine."))
+		return
+
+	if (src.active)
+		user.visible_message(SPAN_NOTICE("[icon2html(src, viewers(get_turf(src)))] [user] deactivates the shield generator."), \
+			SPAN_NOTICE("[icon2html(src, viewers(get_turf(src)))] You deactivate the shield generator."), \
+			"You hear heavy droning fade out.")
+		src.shields_down()
+	else
+		if(anchored)
+			user.visible_message(SPAN_NOTICE("[icon2html(src, viewers(get_turf(src)))] [user] activate the shield generator."), \
+				SPAN_NOTICE("[icon2html(src, viewers(get_turf(src)))] You activate the shield generator."), \
+				"You hear heavy droning.")
+			src.shields_up()
+		else
+			to_chat(user, SPAN_WARNING("The device must first be secured to the floor."))
+	return
+
+/obj/machinery/shieldgen/emag_act(var/remaining_charges, var/mob/user)
+	if(!malfunction)
+		malfunction = TRUE
+		update_icon()
+		return TRUE
+
+/obj/machinery/shieldgen/attackby(obj/item/attacking_item, mob/user)
+	if(attacking_item.tool_behaviour == TOOL_SCREWDRIVER)
+		update_icon()
+		attacking_item.play_tool_sound(get_turf(src), 50)
+		if(panel_open)
+			to_chat(user, SPAN_NOTICE("You close the panel."))
+			panel_open = FALSE
+		else
+			to_chat(user, SPAN_NOTICE("You open the panel and expose the wiring."))
+			panel_open = TRUE
+
+	else if(attacking_item.tool_behaviour == TOOL_CABLECOIL && malfunction && panel_open)
+		var/obj/item/stack/cable_coil/coil = attacking_item
+		to_chat(user, SPAN_NOTICE("You begin to replace the wires."))
+		//if(do_after(user, min(60, round( ((maxhealth/health)*10)+(malfunction*10) ))) //Take longer to repair heavier damage
+		if(attacking_item.use_tool(src, user, 30, volume = 50))
+			if (coil.use(1))
+				health = initial(health)
+				malfunction = FALSE
+				to_chat(user, SPAN_NOTICE("You repair the [src]!"))
+				update_icon()
+
+	else if(attacking_item.tool_behaviour == TOOL_WRENCH)
+		if(locked)
+			playsound(src, 'sound/machines/terminal/terminal_error.ogg', 25, FALSE)
+			balloon_alert(user, "locked!")
+			return
+		if(attacking_item.use_tool(src, user, 1 SECONDS, volume = 50))
+			anchored = !anchored
+			add_fingerprint(user)
+			var/others_msg = anchored ? "<b>[user]</b> secures the external reinforcing bolts to the floor." : "<b>[user]</b> unsecures the external reinforcing bolts."
+			var/self_msg = anchored ? "You secure the external reinforcing bolts to the floor." : "You unsecure the external reinforcing bolts."
+			user.visible_message(others_msg, SPAN_NOTICE(self_msg), SPAN_NOTICE("You hear a ratcheting noise."))
+			update_icon()
+			return
+
+	else
+		..()
+
+/obj/machinery/shieldgen/AltClick(mob/user)
+	if(Adjacent(user))
+		add_fingerprint(user)
+		if(allowed(user))
+			locked = !locked
+			if(locked)
+				playsound(src, 'sound/machines/terminal/terminal_button03.ogg', 35, FALSE)
+			else
+				playsound(src, 'sound/machines/terminal/terminal_button01.ogg', 35, FALSE)
+			balloon_alert(user, locked ? "locked" : "unlocked")
+		else
+			to_chat(user, SPAN_WARNING("Access denied."))
+			playsound(src, 'sound/machines/terminal/terminal_error.ogg', 25, FALSE)
+			balloon_alert(user, "access denied!")
+
+/obj/machinery/shieldgen/update_icon()
+	ClearOverlays()
+	if(anchored)
+		AddOverlays("+bolts")
+	if(active && !(stat & NOPOWER))
+		src.icon_state = malfunction ? "shieldonbr":"shieldon"
+	else
+		src.icon_state = malfunction ? "shieldoffbr":"shieldoff"
+
 /obj/machinery/shield
 	name = "emergency energy shield"
 	desc = "An energy shield used to contain hull breaches."
@@ -8,9 +260,12 @@
 	anchored = TRUE
 	unacidable = TRUE
 	atmos_canpass = CANPASS_NEVER
-	var/health = 75 //The shield can only take so much beating (prevents perma-prisons)
-	var/shield_generate_power = 2500	//how much power we use when regenerating
-	var/shield_idle_power = 500		//how much power we use when just being sustained.
+	/// The shield can only take so much beating (prevents perma-prisons)
+	var/health = 75
+	/// How much power we use when first creating the shield
+	var/shield_generate_power = 75 KILO WATTS
+	/// How much power we use when just being sustained.
+	var/shield_idle_power = 30 KILO WATTS
 
 /obj/machinery/shield/malfai
 	name = "emergency forcefield"
@@ -90,8 +345,12 @@
 
 	health -= hitting_projectile.get_structure_damage()
 	check_failure()
-	opacity = 1
-	spawn(20) if(src) opacity = FALSE
+	opacity = TRUE
+	addtimer(CALLBACK(src, PROC_REF(update_opacity), FALSE), 2 SECONDS)
+
+/obj/machinery/shield/proc/update_opacity(var/new_opacity)
+	if(src)
+		opacity = new_opacity
 
 /obj/machinery/shield/ex_act(severity)
 	switch(severity)
@@ -117,7 +376,6 @@
 			if(prob(50))
 				qdel(src)
 
-
 /obj/machinery/shield/hitby(atom/movable/hitting_atom, skipcatch, hitpush, blocked, datum/thrownthing/throwingdatum)
 	//Let everyone know we've been hit!
 	visible_message(SPAN_NOTICE("<B>\[src] was hit by [hitting_atom].</B>"))
@@ -139,247 +397,7 @@
 
 	//The shield becomes dense to absorb the blow.. purely asthetic.
 	opacity = TRUE
-	spawn(20) if(src) opacity = FALSE
+	addtimer(CALLBACK(src, PROC_REF(update_opacity), FALSE), 2 SECONDS)
 
 	..()
-	return
-
-/obj/machinery/shieldgen
-	name = "emergency shield projector"
-	desc = "Used to seal minor hull breaches."
-	icon = 'icons/obj/machinery/shielding.dmi'
-	icon_state = "shieldoff"
-	density = TRUE
-	opacity = FALSE
-	anchored = FALSE
-	req_access = list(ACCESS_ENGINE)
-	var/health = 100
-	var/active = FALSE
-	// Malfunction causes parts of the shield to slowly dissapate
-	var/malfunction = FALSE
-	var/list/deployed_shields = list()
-	var/list/regenerating = list()
-	panel_open = FALSE
-	var/locked = FALSE
-	var/check_delay = 60	//periodically recheck if we need to rebuild a shield
-	use_power = POWER_USE_OFF
-	idle_power_usage = 0
-
-/obj/machinery/shieldgen/mechanics_hints(mob/user, distance, is_adjacent)
-	. += ..()
-	. += "ALT-click the [src] to lock or unlock it (if you have the appropriate ID access)."
-
-/obj/machinery/shieldgen/Destroy()
-	collapse_shields()
-	return ..()
-
-/obj/machinery/shieldgen/proc/shields_up()
-	if(active) return FALSE //If it's already turned on, how did this get called?
-
-	src.active = 1
-	update_icon()
-
-	create_shields()
-
-	var/shield_power_usage
-	for(var/obj/machinery/shield/shield_tile in deployed_shields)
-		shield_power_usage += shield_tile.shield_idle_power
-
-	change_power_consumption(shield_power_usage, POWER_USE_IDLE)
-	update_use_power(POWER_USE_IDLE)
-
-/obj/machinery/shieldgen/proc/shields_down()
-	if(!active) return FALSE //If it's already off, how did this get called?
-
-	src.active = FALSE
-	update_icon()
-
-	collapse_shields()
-
-	update_use_power(POWER_USE_OFF)
-
-/obj/machinery/shieldgen/proc/create_shields()
-	for(var/T in RANGE_TURFS(2, src))
-		var/turf/target_tile = T
-		if(locate(/obj/machinery/shield) in target_tile)
-			continue
-		var/obj/item/tape/engineering/E = locate() in target_tile
-		if(E?.shield_marker)
-			deploy_shield(target_tile)
-		else if(istype(target_tile,/turf/space) || istype(target_tile,/turf/simulated/open) || istype(target_tile,/turf/simulated/floor/exoplanet/asteroid/ash) || istype(target_tile,/turf/simulated/floor/airless))
-			if(malfunction && prob(33) || !malfunction)
-				deploy_shield(target_tile)
-
-/obj/machinery/shieldgen/proc/deploy_shield(var/turf/T)
-	var/obj/machinery/shield/S = new /obj/machinery/shield(T)
-	deployed_shields += S
-	use_power_oneoff(S.shield_generate_power)
-
-/obj/machinery/shieldgen/proc/collapse_shields()
-	for(var/obj/machinery/shield/shield_tile in deployed_shields)
-		qdel(shield_tile)
-
-/obj/machinery/shieldgen/power_change()
-	..()
-	if(!active) return
-	if (stat & NOPOWER)
-		collapse_shields()
-	else
-		create_shields()
-	update_icon()
-
-/obj/machinery/shieldgen/process()
-	if (!active || (stat & NOPOWER))
-		return
-
-	if(malfunction)
-		if(deployed_shields.len && prob(5))
-			qdel(pick(deployed_shields))
-	else
-		if (check_delay <= 0)
-			create_shields()
-
-			var/new_power_usage = 0
-			for(var/obj/machinery/shield/shield_tile in deployed_shields)
-				new_power_usage += shield_tile.shield_idle_power
-
-			if (new_power_usage != idle_power_usage)
-				change_power_consumption(new_power_usage, POWER_USE_IDLE)
-				use_power_oneoff(0)
-
-			check_delay = 60
-		else
-			check_delay--
-
-/obj/machinery/shieldgen/proc/checkhp()
-	if(health <= 30)
-		src.malfunction = 1
-	if(health <= 0)
-		spawn(0)
-			explosion(get_turf(src.loc), 0, 0, 1, 0, 0, 0)
-		qdel(src)
-	update_icon()
-	return
-
-/obj/machinery/shieldgen/ex_act(severity)
-	switch(severity)
-		if(1.0)
-			src.health -= 75
-			src.checkhp()
-		if(2.0)
-			src.health -= 30
-			if (prob(15))
-				src.malfunction = 1
-			src.checkhp()
-		if(3.0)
-			src.health -= 10
-			src.checkhp()
-	return
-
-/obj/machinery/shieldgen/emp_act(severity)
-	. = ..()
-
-	switch(severity)
-		if(EMP_HEAVY)
-			src.health /= 2 //cut health in half
-			malfunction = TRUE
-			locked = pick(0,1)
-		if(EMP_LIGHT)
-			if(prob(50))
-				src.health *= 0.3 //chop off a third of the health
-				malfunction = TRUE
-	checkhp()
-
-/obj/machinery/shieldgen/attack_hand(mob/user)
-	if(locked)
-		to_chat(user, SPAN_WARNING("The machine is locked!"))
-		return
-	if(panel_open)
-		to_chat(user, SPAN_WARNING("The panel must be closed before operating this machine."))
-		return
-
-	if (src.active)
-		user.visible_message(SPAN_NOTICE("[icon2html(src, viewers(get_turf(src)))] [user] deactivates the shield generator."), \
-			SPAN_NOTICE("[icon2html(src, viewers(get_turf(src)))] You deactivate the shield generator."), \
-			"You hear heavy droning fade out.")
-		src.shields_down()
-	else
-		if(anchored)
-			user.visible_message(SPAN_NOTICE("[icon2html(src, viewers(get_turf(src)))] [user] activate the shield generator."), \
-				SPAN_NOTICE("[icon2html(src, viewers(get_turf(src)))] You activate the shield generator."), \
-				"You hear heavy droning.")
-			src.shields_up()
-		else
-			to_chat(user, SPAN_WARNING("The device must first be secured to the floor."))
-	return
-
-/obj/machinery/shieldgen/emag_act(var/remaining_charges, var/mob/user)
-	if(!malfunction)
-		malfunction = TRUE
-		update_icon()
-		return 1
-
-/obj/machinery/shieldgen/attackby(obj/item/attacking_item, mob/user)
-	if(attacking_item.tool_behaviour == TOOL_SCREWDRIVER)
-		attacking_item.play_tool_sound(get_turf(src), 50)
-		if(panel_open)
-			to_chat(user, SPAN_NOTICE("You close the panel."))
-			panel_open = FALSE
-		else
-			to_chat(user, SPAN_NOTICE("You open the panel and expose the wiring."))
-			panel_open = TRUE
-
-	else if(attacking_item.tool_behaviour == TOOL_CABLECOIL && malfunction && panel_open)
-		var/obj/item/stack/cable_coil/coil = attacking_item
-		to_chat(user, SPAN_NOTICE("You begin to replace the wires."))
-		//if(do_after(user, min(60, round( ((maxhealth/health)*10)+(malfunction*10) ))) //Take longer to repair heavier damage
-		if(attacking_item.use_tool(src, user, 30, volume = 50))
-			if (coil.use(1))
-				health = initial(health)
-				malfunction = FALSE
-				to_chat(user, SPAN_NOTICE("You repair the [src]!"))
-				update_icon()
-
-	else if(attacking_item.tool_behaviour == TOOL_WRENCH)
-		if(locked)
-			to_chat(user, "The bolts are covered, unlocking this would retract the covers.")
-			return
-		if(anchored)
-			attacking_item.play_tool_sound(get_turf(src), 100)
-			to_chat(user, SPAN_NOTICE("You unsecure the [src] from the floor!"))
-			if(active)
-				to_chat(user, SPAN_NOTICE("The [src] shuts off!"))
-				src.shields_down()
-			anchored = FALSE
-		else
-			if(istype(get_turf(src), /turf/space)) return //No wrenching these in space!
-			attacking_item.play_tool_sound(get_turf(src), 100)
-			to_chat(user, SPAN_NOTICE("You secure the [src] to the floor!"))
-			anchored = TRUE
-
-	else
-		..()
-
-/obj/machinery/shieldgen/AltClick(mob/user)
-	if(Adjacent(user))
-		add_fingerprint(user)
-		if(allowed(user))
-			locked = !locked
-			if(locked)
-				playsound(src, 'sound/machines/terminal/terminal_button03.ogg', 35, FALSE)
-			else
-				playsound(src, 'sound/machines/terminal/terminal_button01.ogg', 35, FALSE)
-			balloon_alert(user, locked ? "locked" : "unlocked")
-		else
-			to_chat(user, SPAN_WARNING("Access denied."))
-			playsound(src, 'sound/machines/terminal/terminal_error.ogg', 25, FALSE)
-			balloon_alert(user, "access denied!")
-		return
-
-
-/obj/machinery/shieldgen/update_icon()
-	if(active && !(stat & NOPOWER))
-		src.icon_state = malfunction ? "shieldonbr":"shieldon"
-	else
-		src.icon_state = malfunction ? "shieldoffbr":"shieldoff"
 	return

code/ZAS/Atom.dm
@@ -1,3 +1,4 @@
+/// Pre-check proc for passing things over the turf. Passes more advanced checks for AMs to turf/enter(...), otherwise handles ZAS.
 /turf/CanPass(atom/movable/mover, turf/target, height=1.5,air_group=0)
 	if(!target)
 		return FALSE
@@ -10,31 +11,34 @@
 
 	else // Now, doing more detailed checks for air movement and air group formation
 		if(target.blocks_air||blocks_air)
-			return 0
+			return FALSE
 
 		for(var/obj/obstacle in src)
 			if(!obstacle.CanPass(mover, target, height, air_group))
-				return 0
+				return FALSE
 		if(target != src)
 			for(var/obj/obstacle in target)
 				if(!obstacle.CanPass(mover, src, height, air_group))
-					return 0
+					return FALSE
 
-		return 1
+		return TRUE
 
-//Convenience function for atoms to update turfs they occupy
+/// Convenience function for atoms to update turfs they occupy
 /atom/movable/proc/update_nearby_tiles(need_rebuild)
 	for(var/turf/simulated/turf in locs)
 		SSair.mark_for_update(turf)
 
 	return 1
 
-//Basically another way of calling CanPass(null, other, 0, 0) and CanPass(null, other, 1.5, 1).
-//Returns:
-// 0 - Not blocked
-// AIR_BLOCKED - Blocked
-// ZONE_BLOCKED - Not blocked, but zone boundaries will not cross.
-// BLOCKED - Blocked, zone boundaries will not cross even if opened.
+/**
+ * Basically another way of calling CanPass(null, other, 0, 0) and CanPass(null, other, 1.5, 1).
+ *
+ * Returns:
+ * - 0 - Not blocked
+ * - AIR_BLOCKED - Blocked
+ * - ZONE_BLOCKED - Not blocked, but zone boundaries will not cross.
+ * - BLOCKED - Blocked, zone boundaries will not cross even if opened.
+ */
 /atom/proc/c_airblock(turf/other)
 	#ifdef ZASDBG
 	ASSERT(isturf(other))

code/controllers/subsystems/falling.dm
@@ -4,6 +4,19 @@
 	if (MC_TICK_CHECK) return;		\
 	continue;
 
+/**
+ * Notes for potential future development.
+ *
+ * Falling subsystem currently treats 'falling' as a event that happens instantly in the presence of gravity, which (presently) is a binary true/false condition.
+ * To handle more strengths of gravity beyond 0g and 1g, falling logic will need to be able to handle things falling 'slowly'. The easiest way to implement this
+ * will probably be to provide handling for an component/element (if pending fall in low-g, give an AM an additional highlight or examine-able info), and also
+ * provide in-game feedback in some way to the player- ideally through a UI grav condition button like /tg/'s, that indicates how long it'll take before you drift
+ * down a Z-level.
+ *
+ * This would also give us more granular control over levels of sub-1 gravity. Instead of 'no grav, low grav, standard grav', we could have 0.0 -> 1.0 grav just be
+ * the the delay factor on the falling subsystem's fire() logic, and the thresholds over which that a z-level fall happens gently (or not gently).
+ */
+
 SUBSYSTEM_DEF(falling)
 	name = "Falling"
 	flags = SS_NO_INIT

commit 5adff1b2ec958a3535b083985fe096e999b4c5cb

Emergency Shield Projector activation guard logic (#22016)
Fixes a bug introduced in
https://github.com/Aurorastation/Aurora.3/pull/21862 that caused shield
projectors to activate during certain delayed process() ticks and when
dragged across an area boundary (only noticeable if doing so within the
radius of open space turfs).

code/modules/shieldgen/emergency_shield.dm
@@ -48,6 +48,8 @@
 	change_power_consumption(shield_power_usage, POWER_USE_IDLE)
 	update_use_power(POWER_USE_IDLE)
 
+	return TRUE
+
 /obj/machinery/shieldgen/proc/shields_down()
 	if(!active) return FALSE //If it's already off, how did this get called?
 
@@ -88,8 +90,6 @@
 	if(!active) return
 	if(stat & NOPOWER)
 		collapse_shields()
-	else
-		create_shields()
 	update_icon()
 
 /obj/machinery/shieldgen/process()
@@ -104,18 +104,17 @@
 		if(deployed_shields.len && prob(5))
 			qdel(pick(deployed_shields))
 	else
-		if (check_delay <= 0)
-			create_shields()
+		if(check_delay <= 0)
+			if(shields_up())
+				var/new_power_usage = 0
+				for(var/obj/machinery/shield/shield_tile in deployed_shields)
+					new_power_usage += shield_tile.shield_idle_power
 
-			var/new_power_usage = 0
-			for(var/obj/machinery/shield/shield_tile in deployed_shields)
-				new_power_usage += shield_tile.shield_idle_power
+				if (new_power_usage != idle_power_usage)
+					change_power_consumption(new_power_usage, POWER_USE_IDLE)
+					use_power_oneoff(0)
 
-			if (new_power_usage != idle_power_usage)
-				change_power_consumption(new_power_usage, POWER_USE_IDLE)
-				use_power_oneoff(0)
-
-			check_delay = 30
+				check_delay = 30
 		else
 			check_delay--
 