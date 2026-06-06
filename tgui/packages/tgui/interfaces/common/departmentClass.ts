cde601b02120783244b0c7ad0a0b32bb833cfbe1
Batrachophreno <Batrochophreno@gmail.com>
2026-06-06 18:16:53 -0400

    TGUI Fixes, Did You Think I Was Done Edition (#22641)
    Fixes https://github.com/Aurorastation/Aurora.3/issues/15357
    Fixes https://github.com/Aurorastation/Aurora.3/issues/22459
    Fixes https://github.com/Aurorastation/Aurora.3/issues/22593
    Fixes https://github.com/Aurorastation/Aurora.3/issues/22634
    
    changes:
    - bugfix: "Restores chat highlight background color selection
    functionality (PR #20701)"
      - bugfix: "Restores customizable chat message retention settings."
      - bugfix: "Updates recommended vscode extension config file."
    - bugfix: "Corrects some mismapped and missing scss files as well as
    removing temporary working files leftover in PR."
    - bugfix: "Fixes silicons/AI being unable to interact remotely with
    shield capacitor and shield generator machinery."
    - bugfix: "Fixes gene data disks produced by xenobotany's
    lysis-isolation centrifuge machine not displaying gene names."
      - bugfix: "Makes AI remote access to machinery TGUIs more consistent."
    - bugfix: "Fixes the Sensors interface not displaying sensor machinery's
    current health."
    - bugfix: "Fixes the Sensors interface displaying strange values for
    current range."
    - bugfix: "Fixes modular computer software displaying device themes
    instead of explicit software themes."
      - bugfix: "Fixes MC tab links."
      - spellcheck: "Fixes a typo in the TGUI fatal error message."

--- tgui/packages/tgui/interfaces/common/departmentClass.ts
+++ tgui/packages/tgui/interfaces/common/departmentClass.ts
@@ -1,5 +1,42 @@
-export const departmentClass = (department: string) =>
-  `border-dept-${department
+import type { CSSProperties } from 'react';
+
+import { COLORS } from '../../constants';
+
+type DepartmentStyle = CSSProperties & {
+  '--department-color': string;
+};
+
+const departmentColorByName: Record<string, string> = {
+  cargo: COLORS.department.cargo,
+  operations: COLORS.department.cargo,
+  command: COLORS.department.medbay,
+  'command-support': COLORS.department.medbay,
+  security: COLORS.department.security,
+  engineering: COLORS.department.engineering,
+  medical: COLORS.department.medbay,
+  science: COLORS.department.science,
+  service: COLORS.department.service,
+  civilian: COLORS.department.other,
+  centcom: COLORS.department.centcom,
+  miscellaneous: COLORS.department.other,
+  silicon: COLORS.department.other,
+  equipment: COLORS.department.other,
+  'off-ship': COLORS.department.other,
+  other: COLORS.department.other,
+};
+
+const normalizeDepartment = (department: string) =>
+  department
     .toLowerCase()
     .replace(/[^a-z0-9]+/g, '-')
-    .replace(/^-|-$/g, '')}`;
+    .replace(/^-|-$/g, '');
+
+export const departmentColor = (department: string) =>
+  departmentColorByName[normalizeDepartment(department)] ||
+  COLORS.department.other;
+
+export const departmentClass = () => 'border-dept';
+
+export const departmentStyle = (department: string): DepartmentStyle => ({
+  '--department-color': departmentColor(department),
+});
