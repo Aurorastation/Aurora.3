4a895535e73513aa4ababba0cc1d45ccc8a0df35
mineymonkey <brandontanner63@outlook.com>
2026-07-31 11:55:58 -0500

    Revert "Should fix import/export issues, :hopium:"
    This reverts commit 131fa4b391b0946a4bded91a164dd7133e06430a.

--- tgui/packages/tgui/interfaces/CircuitPrinter.tsx
+++ tgui/packages/tgui/interfaces/CircuitPrinter.tsx
@@ -185,7 +185,7 @@ export const CircuitPrinter = (props) => {
         const chunk = importText.slice(offset, offset + chunkLimit);
 
         await act('append_import_chunk_tgui', {
-          chunk,
+          chunk: encodeURIComponent(chunk),
         });
 
         offset += chunk.length;
