1a9bd786b01fc44d460deebd60e41dd4bf6c7833
runecap <43975590+runecap@users.noreply.github.com>
2026-08-13 16:46:47 -0400

    SQL migrate
    I actually bothered watching some SQL tutorials to understand this syntax, better work

--- SQL/migrate-2017/V047__IPC_tag_project.sql
+++ SQL/migrate-2017/V047__IPC_tag_project.sql
@@ -1,12 +1,13 @@
 --
 -- Reworks how IPC tags work in PR #6973.
 -- This drops the old ss13_ipc_tracking table in favour of ss13_characters_ipc_tags with new fresh information.
--- 
+--
 
 DROP TABLE `ss13_ipc_tracking`;
 
 CREATE TABLE `ss13_characters_ipc_tags` (
   `char_id` int(11) NOT NULL,
+  `custom_model` varchar(20) DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
   `tag_status` tinyint(1) NOT NULL DEFAULT '0',
   `serial_number` varchar(12) DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
   `ownership_status` enum('Self Owned','Company Owned','Privately Owned') DEFAULT 'Company Owned' COLLATE 'utf8mb4_unicode_ci',
